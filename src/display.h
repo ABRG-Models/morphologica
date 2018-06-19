6a526455c4d3d569a971a4aa3c0f72f54148046d
Seb James <seb.james@sheffield.ac.uk>
2018-06-19 11:07:55 +0100

    Minor code tidying

--- src/display.h
+++ src/display.h
@@ -17,18 +17,17 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
-#include "cv.h"
-#include "highgui.h"
+#include <cv.h>
+#include <highgui.h>
+
+using std::vector;
 
 class Gdisplay
 {
-
 private:
-
     XSetWindowAttributes attributes;
 
 public:
-
     Display                 *disp;
     Window                  root;
     XVisualInfo             *vi;
@@ -38,40 +37,40 @@ public:
     GLXContext              glc;
     XWindowAttributes       gwa;
     XEvent                  xev;
-    KeySym					key;
-    char					text[1];
+    KeySym                  key;
+    char                    text[1];
 
     double rho, theta, phi, alpha, speed, X, Y, Z;
     double IK, JL, AD, WS, TG, FH, UO, QE, RY;
 
     Gdisplay(int,const char*, double, double, double);
     void setTitle(char*);
-    void resetDisplay(std::vector <double>, std::vector <double>, std::vector <double>);
+    void resetDisplay(vector <double>, vector <double>, vector <double>);
     void redrawDisplay();
     void closeDisplay();
     void drawHex(double,double,double,double,double,double,double);
     void drawHexSeg(double x,double y,double z,double r,double red,double green,double blue,int q);
-    // void drawTri(std::vector <double>,std::vector <double>,std::vector <double>,double,double,double);
-    void drawTri(std::vector <double>,std::vector <double>,std::vector <double>,std::vector <double>);
-    void drawTriFill(std::vector <double>,std::vector <double>,std::vector <double>,std::vector <double>);
-    void drawSphere(double,double,double,double,std::vector <double>,int);
+    // void drawTri(vector <double>,vector <double>,vector <double>,double,double,double);
+    void drawTri(vector <double>,vector <double>,vector <double>,vector <double>);
+    void drawTriFill(vector <double>,vector <double>,vector <double>,vector <double>);
+    void drawSphere(double,double,double,double,vector <double>,int);
     void drawLine(double,double,double,double,double,double,double,double,double,double);
     void addCrossHairs(double,double,int);
     void addFloor(double,double);
-    // void drawCylinder(std::vector <double>,std::vector <double>,double, double, double, std::vector <double>,int);
-    void addQuad(std::vector <double>,std::vector <double>,std::vector <double>, std::vector <double>,std::vector <double>, std::vector <double>);
+    // void drawCylinder(vector <double>,vector <double>,double, double, double, vector <double>,int);
+    void addQuad(vector <double>,vector <double>,vector <double>, vector <double>,vector <double>, vector <double>);
 
     void saveImage(std::string);
-    void drawCylinder(float, float, float, float, float, float, float, float,int,std::vector <double>);
+    void drawCylinder(float, float, float, float, float, float, float, float,int,vector <double>);
 
-    void drawMesh(std::vector< std::vector< std::vector <double> > >, std::vector<std::vector<std::vector<double> > >);
-    void drawTorus(std::vector< std::vector< std::vector <double> > >, std::vector<std::vector<std::vector<double> > >);
-    void drawMesh2(std::vector< std::vector< std::vector <double> > >, std::vector <double>);
+    void drawMesh(vector< vector< vector <double> > >, vector<vector<vector<double> > >);
+    void drawTorus(vector< vector< vector <double> > >, vector<vector<vector<double> > >);
+    void drawMesh2(vector< vector< vector <double> > >, vector <double>);
 
-    void drawCubeSphere(std::vector< std::vector <double> > );
-    // void drawFlatCube(std::vector< std::vector <double> > );
-    void drawSphereFromMesh(std::vector<std::vector<double> >, std::vector<std::vector<int> >,std::vector<std::vector<double> >);
-    void drawFlatCube(std::vector< std::vector<int> >, std::vector< std::vector<double> >,double,double,double);
+    void drawCubeSphere(vector< vector <double> > );
+    // void drawFlatCube(vector< vector <double> > );
+    void drawSphereFromMesh(vector<vector<double> >, vector<vector<int> >,vector<vector<double> >);
+    void drawFlatCube(vector< vector<int> >, vector< vector<double> >,double,double,double);
 };
 
 #endif /* defined(____display__) */
