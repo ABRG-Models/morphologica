6b55f32bece641e5b8df25646084cb35d857673f
Seb James <seb.james@sheffield.ac.uk>
2019-05-23 16:35:38 +0100

    Starts to add some skeleton code for the rendering of a HexGridVisual.

--- tests/testVisual.cpp
+++ tests/testVisual.cpp
@@ -1,4 +1,5 @@
 #include "Visual.h"
+#include "HexGrid.h"
 #include <utility>
 #include <iostream>
 #include <fstream>
@@ -6,12 +7,30 @@
 
 using namespace std;
 using morph::Visual;
+using morph::HexGrid;
 
 int main()
 {
     int rtn = -1;
 
     Visual v(800,600,"Test window");
+
+    HexGrid hg(2.309401, 4000);
+    cout << "Set up " << hg.num() << " hexes in a grid." << endl;
+
+    vector<float> data;
+    unsigned int nhex = hg.num();
+    data.resize(nhex, 0.0);
+
+    // Make some dummy data
+    for (unsigned int hi=0; hi<nhex; ++hi) {
+        data[hi] = 1.0 * hg.d_x[hi];
+    }
+
+    array<float, 3> offset = { 0.0, 0.0, 0.0 };
+    unsigned int gridId = v.addHexGridVisual (&hg, data, offset);
+
+    v.redraw();
 
     cout << "Enter key to end" << endl;
 
