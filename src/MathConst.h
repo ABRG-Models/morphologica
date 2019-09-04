e907e85bfeaa4a2acb43773468f78acfe69c9ca9
Seb James <seb.james@sheffield.ac.uk>
2019-09-04 13:51:16 +0100

    Pi and Pi/2

--- src/MathConst.h
+++ src/MathConst.h
@@ -12,6 +12,12 @@ namespace morph {
 
     //! Root 3 over 2 as a float
     const float SQRT_OF_3_OVER_2_F = 0.866025404;
+
+    //! PI
+    const float PI_F = 3.14159265358979;
+
+    //! PI/2
+    const float PI_OVER_2_F = 1.5707963267949;
 }
 
 #endif // MATHCONST
