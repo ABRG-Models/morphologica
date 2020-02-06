9dc5787d9b19047dd120aaf2552d4029a75ace82
Seb James <seb.james@sheffield.ac.uk>
2020-02-06 17:43:21 +0000

    Adds double versions of some constants

--- src/MathConst.h
+++ src/MathConst.h
@@ -9,18 +9,23 @@ namespace morph {
 
     //! Square root of 3 as a float
     const float SQRT_OF_3_F = 1.73205081;
+    const double SQRT_OF_3_D = 1.73205080756888;
 
     //! Root 3 over 2 as a float
     const float SQRT_OF_3_OVER_2_F = 0.866025404;
+    const double SQRT_OF_3_OVER_2_D = 0.86602540378444;
 
     //! PI
     const float PI_F = 3.14159265358979;
+    const double PI_D = 3.14159265358979;
 
     //! PI/2
     const float PI_OVER_2_F = 1.5707963267949;
+    const double PI_OVER_2_D = 1.5707963267949;
 
     //! 2*PI as float
     const float TWO_PI_F = 6.28318530717959;
+    const double TWO_PI_D = 6.28318530717959;
 }
 
 #endif // MATHCONST
