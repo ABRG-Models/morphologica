2b647731b83c3a81d2dd038bddd8f7f56e3c7eb7
Seb James <seb.james@sheffield.ac.uk>
2020-04-08 16:04:36 +0100

    Modified as a potential paper figure

--- tests/testbezderiv3.cpp
+++ tests/testbezderiv3.cpp
@@ -1,3 +1,6 @@
+/*
+ * Another test program. This one is for use as a paper figure.
+ */
 #include "HexGrid.h"
 #include "BezCurve.h"
 #include "BezCurvePath.h"
@@ -22,7 +25,9 @@ using morph::HexGrid;
 
 using namespace cv;
 
+// Colours are BGR(A) format
 #define M_BLUE Scalar(255,0,0,10)
+#define M_PURPLE Scalar(255,0,200,10)
 #define M_GREEN Scalar(0,255,0,10)
 #define M_RED Scalar(0,0,255,10)
 #define M_YELLOW Scalar(0,255,255,10)
@@ -31,42 +36,15 @@ using namespace cv;
 #define M_C1 Scalar(238,121,159) // mediumpurple2
 #define M_C2 Scalar(238,58,178) // darkorchid2
 
-void draw (Mat& pImg, BezCurvePath<FLT>& bcp, vector<pair<FLT,FLT>>& v, vector<pair<FLT,FLT>>& w, Scalar linecolour) {
+// Colours for two separate Bezier curves
+#define M_CURVE1 M_BLUE
+#define M_CURVE2 M_C2
+// User control points colour
+#define M_CTRL M_BLACK
+// Fit colour
+#define M_FIT M_RED
 
-    // Add the control points in similar colours
-    list<BezCurve<FLT>> theCurves = bcp.curves;
-    size_t j = 0;
-    for (auto curv : theCurves) {
-        Scalar linecol = j%2 ? M_BLUE : M_GREEN;
-        vector<pair<FLT,FLT>> ctrls = curv.getControls();
-        for (size_t cc = 0; cc<ctrls.size(); ++cc) {
-            Point p1(ctrls[cc].first, ctrls[cc].second);
-            circle (pImg, p1, 5, linecol, 2);
-            if (cc==0 || cc==ctrls.size()-1) {
-                circle (pImg, p1, 2, M_BLACK, -1);
-            } else {
-                circle (pImg, p1, 2, M_WHITE, -1);
-            }
-        }
-        Point ps(ctrls[0].first, ctrls[0].second);
-        Point pe(ctrls[1].first, ctrls[1].second);
-        line (pImg, ps, pe, linecolour, 1);
-        Point ps2(ctrls[ctrls.size()-2].first, ctrls[ctrls.size()-2].second);
-        Point pe2(ctrls[ctrls.size()-1].first, ctrls[ctrls.size()-1].second);
-        line (pImg, ps2, pe2, linecolour, 1);
-        j++;
-    }
-
-    // User controls
-    for (unsigned int i = 0; i < v.size(); ++i) {
-        Point p1(v[i].first, v[i].second);
-        circle (pImg, p1, 2, M_BLACK, -1);
-    }
-
-    for (unsigned int i = 0; i < w.size(); ++i) {
-        Point p1(w[i].first, w[i].second);
-        circle (pImg, p1, 2, M_BLACK, -1);
-    }
+void draw (Mat& pImg, BezCurvePath<FLT>& bcp, vector<pair<FLT,FLT>>& v, vector<pair<FLT,FLT>>& w, Scalar linecolfit, int sz) {
 
     unsigned int nFit = 200;
     vector<Point> fitted (nFit);
@@ -84,26 +62,46 @@ void draw (Mat& pImg, BezCurvePath<FLT>& bcp, vector<pair<FLT,FLT>>& v, vector<p
     }
 
     // This is the fit line.
-    for(size_t i=1; i<fitted.size(); i++) {
-        line (pImg, fitted[i-1], fitted[i], linecolour, 1);
+    for (size_t i=1; i<fitted.size(); i++) {
+        line (pImg, fitted[i-1], fitted[i], linecolfit, 2);
     }
 
-    if (v.empty()) {
-        for(size_t i=0; i<fitted.size(); i++) {
-            Point2d normLen = normals[i]*100.0;
-	    Point2i nli;
-	    nli.x = (int)normLen.x;
-	    nli.y = (int)normLen.y;
-            line (pImg, fitted[i], fitted[i]+nli, linecolour, 1);
+    // Add the control points in similar colours
+    list<BezCurve<FLT>> theCurves = bcp.curves;
+    size_t j = 0;
+    for (auto curv : theCurves) {
+        Scalar linecol = linecolfit; //j%2 ? linecolour1 : linecolour2;
+        vector<pair<FLT,FLT>> ctrls = curv.getControls();
+        // Draw the control points
+        for (size_t cc = 0; cc<ctrls.size(); ++cc) {
+            Point p1(ctrls[cc].first, ctrls[cc].second);
+            circle (pImg, p1, sz, linecol, 2);
+            if (cc==0 || cc==ctrls.size()-1) {
+                circle (pImg, p1, sz-2, M_YELLOW, -1);
+            } else {
+                circle (pImg, p1, sz-2, M_GREEN, -1);
+            }
         }
+        // Draw in the lines to the control points
+        Point ps(ctrls[0].first, ctrls[0].second);
+        Point pe(ctrls[1].first, ctrls[1].second);
+        line (pImg, ps, pe, linecol, 1);
+        Point ps2(ctrls[ctrls.size()-2].first, ctrls[ctrls.size()-2].second);
+        Point pe2(ctrls[ctrls.size()-1].first, ctrls[ctrls.size()-1].second);
+        line (pImg, ps2, pe2, linecol, 1);
+        j++;
     }
 
-    circle (pImg, Point(10,10), 2, M_BLACK, -1);
-    circle (pImg, Point(1600,10), 2, M_BLACK, -1);
-    circle (pImg, Point(1600,1000), 2, M_BLACK, -1);
-    circle (pImg, Point(800,10), 2, M_BLACK, -1);
-    circle (pImg, Point(800,1000), 2, M_BLACK, -1);
-    circle (pImg, Point(10,1000), 2, M_BLACK, -1);
+    // The user control points
+    for (unsigned int i = 0; i < v.size(); ++i) {
+        Point p1(v[i].first, v[i].second);
+        circle (pImg, p1, 5, M_CTRL, -1);
+    }
+
+    for (unsigned int i = 0; i < w.size(); ++i) {
+        Point p1(w[i].first, w[i].second);
+        circle (pImg, p1, 5, M_CTRL, -1);
+    }
 }
 
 int main (int argc, char** argv)
@@ -120,16 +118,18 @@ int main (int argc, char** argv)
 
     int rtn = 0;
     vector<pair<FLT,FLT>> v;
-    v.push_back (make_pair (200,500));
-    v.push_back (make_pair (300,450));
-    v.push_back (make_pair (400,400));
-    v.push_back (make_pair (450,300));
+    FLT fac = 2.2;
+    FLT xoff = -300.0;
+    v.push_back (make_pair (xoff+fac*200,fac*500));
+    v.push_back (make_pair (xoff+fac*300,fac*450));
+    v.push_back (make_pair (xoff+fac*400,fac*400));
+    v.push_back (make_pair (xoff+fac*450,fac*300));
 
     vector<pair<FLT,FLT>> w;
     w.push_back (v.back());
-    w.push_back (make_pair (500,200));
-    w.push_back (make_pair (580,50));
-    w.push_back (make_pair (630,20));
+    w.push_back (make_pair (xoff+fac*500,fac*240));
+    w.push_back (make_pair (xoff+fac*580,fac*30));
+    w.push_back (make_pair (xoff+fac*630,fac*20));
 
     // First the analytical fit
     BezCurve<FLT> cv1;
@@ -142,11 +142,11 @@ int main (int argc, char** argv)
     bcp.addCurve (cv2);
 
     // Create a frame as the background for our drawing.
-    Mat frame = Mat (800, 1000, CV_8UC3, M_WHITE);
+    Mat frame = Mat (1200, 1200, CV_8UC3, M_WHITE);
 
     // Draw
     cout << "Draw the two analytical best-fit curves..." << endl;
-    draw (frame, bcp, v, w, M_RED);
+    draw (frame, bcp, v, w, M_BLUE, 12);
 
     cout << "Do the control point-equalizing 0th order optimization..."<< endl;
     bool withopt = false;
@@ -157,10 +157,9 @@ int main (int argc, char** argv)
     bcp.addCurve (cv1);
     bcp.addCurve (cv2);
 
-    //v.clear();
-    //w.clear();
-    draw (frame, bcp, v, w, M_BLUE);
+    draw (frame, bcp, v, w, M_RED, 8);
 
+#if 0
     // Reset and fit with optimzation
     withopt = true;
     // Reset the best fits:
@@ -174,10 +173,9 @@ int main (int argc, char** argv)
     bcp.addCurve (cv1);
     bcp.addCurve (cv2);
 
-    //v.clear();
-    //w.clear();
-    draw (frame, bcp, v, w, M_GREEN);
-    cout << "Semi-optimised is BLUE; Fully optimized is GREEN" << endl;
+    //draw (frame, bcp, v, w, M_FIT);
+    //cout << "Semi-optimised is BLUE; Fully optimized is GREEN" << endl;
+#endif
 
     namedWindow( "Curves", WINDOW_AUTOSIZE );// Create a window for display.
     imshow ("Curves", frame);
