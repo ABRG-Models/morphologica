e56dd9b11dbceae5d2041eeb13e052bfb893d880
Seb James <seb.james@sheffield.ac.uk>
2020-04-18 18:56:20 +0100

    Adds two additional test/example programs

---
+++ tests/testScaleVector.cpp
@@ -0,0 +1,59 @@
+/*
+ * Test use of Vector<F, N> objects in a Scale class.
+ */
+
+#include <vector>
+using std::vector;
+#include <list>
+using std::list;
+#include <array>
+using std::array;
+#include <iostream>
+using std::cout;
+using std::endl;
+#include "Vector.h"
+using morph::Vector;
+#include "Scale.h"
+using morph::Scale;
+
+int main () {
+
+    int rtn = 0;
+
+    Scale<Vector<float,4>> s2;
+    s2.do_autoscale = true;
+    vector<Vector<float,4>> vaf;
+    vaf.push_back ({1,1,2,1});
+    vaf.push_back ({2,2,2,3});
+    vaf.push_back ({3,3,4,1});
+    vaf.push_back ({4,4,4,4});
+    vector<Vector<float,4>> result2(vaf);
+    s2.transform (vaf, result2);
+
+    cout << "vector<Vector<float,4>> unscaled/scaled vectors:\n";
+    for (unsigned int i = 0; i < result2.size(); ++i) {
+
+        cout << "(";
+        for (auto v : vaf[i]) {
+            cout << v << ",";
+        }
+        cout << ")   ";
+
+        cout << "(";
+        for (auto v : result2[i]) {
+            cout << v << ",";
+        }
+        cout << ")\n";
+    }
+
+    // Test this scaling:
+    vector<Vector<float, 4>>::const_iterator r2i = result2.end();
+    r2i--; // To get to last element in vector
+    float r2ilen = std::sqrt ((*r2i)[0] * (*r2i)[0] + (*r2i)[1] * (*r2i)[1] + (*r2i)[2] * (*r2i)[2] + (*r2i)[3] * (*r2i)[3]);
+    if (abs(r2ilen - 1) > 0.0001) {
+        cout << "Error" << endl;
+        rtn--;
+    }
+
+    return rtn;
+}
---
+++ tests/testVector14.cpp
@@ -0,0 +1,89 @@
+#include "Vector.h"
+using morph::Vector;
+
+int main() {
+    int rtn = 0;
+    Vector<float, 3> v;
+    v[0] = 1.0f;
+    v[1] = 2.0f;
+    v[2] = 3.0f;
+    // Test x(), y() etc members
+    cout << "x: " << v.x() << endl;
+    cout << "z: " << v.z() << endl;
+    //cout << "w: " << v.w() << endl;
+    // Test output
+    v.output(); cout << endl;
+    // Test renormalize
+    v.renormalize();
+    cout << "After renormalize: "; v.output(); cout << endl;
+    // Test checkunit
+    cout << "is it unit? " << v.checkunit() << endl;
+    // Test randomize
+    v.randomize();
+    cout << "After randomize: "; v.output(); cout << endl;
+    // Check ints are ok, too
+    Vector<int, 2> vi;
+    vi.randomize();
+    cout << "After randomize of int vector: "; vi.output(); cout << endl;
+    cout << "Length: " << vi.length() << endl;
+    // Test assignment
+    Vector<int, 2> vi2 = vi;
+    cout << "Copy of int vector: "; vi2.output(); cout << endl;
+    // Test comparison
+    cout << "vi == vi2? " << (vi == vi2 ? "yes" : "no") << endl;
+    // Test negate
+    Vector<int, 2> vi3 = -vi;
+    cout << "-ve Copy of int vector: "; vi3.output(); cout << endl;
+    // Test comparison
+    cout << "vi == vi3? " << (vi == vi3 ? "yes" : "no") << endl;
+    // Test cross product (3D only
+    Vector<double, 3> a; a[0] = 1;
+    Vector<double, 3> b; b[1] = 1;
+    Vector<double, 3> c = a * b;
+    cout << a << "*" << b << "=" << c << endl;
+    // Test dot product
+    Vector<int, 2> vv1; vv1[0]=1; vv1[1]=1;
+    Vector<int, 2> vv2; vv2[0]=2; vv2[1]=2;
+    int dp = vv1.dot (vv2);
+    cout << vv1 << "." << vv2 << " = " << dp << endl;
+#if 0 // No good:
+    // Test init from array
+    array<float, 3> arr = { 2,3,4 };
+    //Vector<float, 3> varr(arr);
+    Vector<float, 3> varr = arr;
+    cout << "Vector from array: " << varr << endl;
+#endif
+    // Test scalar multiply
+    vv2 *= 2UL;
+    cout << "vv2 after *2:" << vv2 << endl;
+    Vector<int, 2> vv4 = vv1 * 98;
+    cout << "vv1 * 98:" << vv4 << endl;
+    // Scalar division
+    Vector<double, 3> d = a/3.0;
+    cout << "a/3.0:" << d << endl;
+    // Vector addition
+    Vector<double, 3> e = a+b;
+    cout << "a+b:" << e << endl;
+    // Vector subtraction
+    Vector<double, 3> f = a-b;
+    cout << "a-b:" << f << endl;
+    // Test default template args
+    Vector<double> vd_def;
+    vd_def.randomize();
+    cout << vd_def << endl;
+#if 0 // no good in c++-11 or c++-14
+    Vector v_def;
+    v_def.randomize();
+    cout << v_def << endl;
+#endif
+    // So you want to do the dot product of a 1000000 D vector? Easy
+    Vector<float, 1000> big1;
+    Vector<float, 1000> big2;
+    big1.randomize();
+    big2.randomize();
+    cout << "DP..." << endl;
+    float bdp = big1.dot(big2);
+    cout << "big1.big2=" << bdp << endl;
+
+    return rtn;
+}
