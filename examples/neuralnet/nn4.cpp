5e78327447f7f4f5f1376008e4f02295de9a81ba
Seb James <seb.james@sheffield.ac.uk>
2020-05-10 11:02:41 +0100

    but the Mnist characterization still doesn't work

--- examples/neuralnet/nn4.cpp
+++ examples/neuralnet/nn4.cpp
@@ -11,49 +11,50 @@ int main()
     // Instantiate the network (What would be really cool would be a FeedForwardNet as a
     // variadic template, so that FeedForwardNet<float, 785, 15, 10> ff1 would create
     // the network the right way)
-    std::vector<unsigned int> layer_spec = {784,30,10};
+    std::vector<unsigned int> layer_spec = {784,31,10};
     FeedForwardNetS<float> ff1(layer_spec);
 
     // Create a random number generator
     morph::RandUniform<unsigned char> rng((unsigned char)0, (unsigned char)9);
 
-    // main loop, while m.training_f has values in:
-    unsigned int epochs = 1;
-    unsigned int mini_batch_size = 1;
+    // main loop parameters are number of epochs, the size of a mini-batch and the
+    // learning rate eta
+    unsigned int epochs = 30;
+    unsigned int mini_batch_size = 30;
     float eta = 3.0f;
-    float cost = 0.0f;
 
-    // Accumulate the dC/dw and dC/db values in graidents. for each pair, the first
-    // is nabla_w the second is nabla_b. There are as many pairs as there are
-    // connections in ff1.
+    // Accumulate the dC/dw and dC/db values in gradients. for each pair, the first is
+    // nabla_w the second is nabla_b. There are as many pairs as there are connections
+    // in ff1. Here, we declare an initialize mean_gradients
     std::vector<std::pair<morph::vVector<float>, morph::vVector<float>>> mean_gradients;
-    // Init mean gradients
     for (auto& c : ff1.connections) {
         mean_gradients.push_back (std::make_pair(c.nabla_w, c.nabla_b));
     }
 
+    // Open a file to output costs into (for making a graph)
     std::ofstream costfile;
     costfile.open ("cost.csv", std::ios::out|std::ios::trunc);
 
-    // Used throughout as an iterator variable
+    // i is used as an iterator variable throughout the loop below
     unsigned int i = 0;
+
     for (unsigned int ep = 0; ep < epochs; ++ep) {
 
-        // Copy out the training data:
-        std::multimap<unsigned char,
-                      morph::vVector<float>> training_f = m.training_f;
-#if 1
-        for (unsigned int j = 0; j < 2/*training_f.size()/mini_batch_size*/; ++j) {
-#else
+        // At start of epoch, make a copy of the training data:
+        std::multimap<unsigned char, morph::vVector<float>> training_f = m.training_f;
+
         for (unsigned int j = 0; j < training_f.size()/mini_batch_size; ++j) {
-#endif
-            // Zero mean grads
+
+            // Update the mini-batch
+
+            // Zero mean grads and cost
             for (i = 0; i < ff1.num_connection_layers(); ++i) {
                 mean_gradients[i].first.zero();
                 mean_gradients[i].second.zero();
             }
+            float cost = 0.0f;
 
-            cost = 0.0f;
+            // Loop through each member of the mini-batch
             for (unsigned int mb = 0; mb < mini_batch_size; ++mb) {
 
                 // Set up input
@@ -75,7 +76,7 @@ int main()
                 i = 0;
                 for (auto& c : ff1.connections) {
 #if 0
-                    std::cout << "During accumulation):\n";
+                    std::cout << "Gradients during accumulation:\n";
                     std::cout << "layer " << i << ", nabla_w: " << c.nabla_w << std::endl;
                     std::cout << "      " << i << ", nabla_b: " << c.nabla_b << std::endl;
 #endif
@@ -85,7 +86,7 @@ int main()
                 }
             }
 #if 0
-            std::cout << "Before division (after accumulation):\n";
+            std::cout << "Gradients before division (but after accumulation):\n";
             for (i = 0; i < ff1.num_connection_layers(); ++i) {
                 std::cout << "layer " << i << ", nabla_w: " << mean_gradients[i].first << std::endl;
                 std::cout << "      " << i << ", nabla_b: " << mean_gradients[i].second << std::endl;
@@ -96,11 +97,11 @@ int main()
                 mean_gradients[i].first /= static_cast<float>(mini_batch_size);
                 mean_gradients[i].second /= static_cast<float>(mini_batch_size);
             }
-            cost /= (2.0f*static_cast<float>(mini_batch_size));
+            cost /= static_cast<float>(mini_batch_size);
             costfile << cost << std::endl;
             //std::cout << cost << std::endl;
 #if 0
-            std::cout << "After accumulation and division:\n";
+            std::cout << "Gradients after accumulation and division:\n";
             for (i = 0; i < ff1.num_connection_layers(); ++i) {
                 std::cout << "layer " << i << ", nabla_w: " << mean_gradients[i].first << std::endl;
                 std::cout << "      " << i << ", nabla_b: " << mean_gradients[i].second << std::endl;
@@ -118,10 +119,18 @@ int main()
 #if 0
             std::cout << "After gradient alteration ff1:\n---------------\n" << ff1 << std::endl;
 #endif
+#if 0
+            unsigned int numcorrect = ff1.evaluate (m.test_f, 10);
+            std::cout << "In that last minibatch, "<< numcorrect << "/10 were characterized correctly" << std::endl;
+            std::cout << ff1;
+#endif
         }
+#if 1
         // Evaluate for that epoch
         unsigned int numcorrect = ff1.evaluate (m.test_f);
         std::cout << "In that last Epoch, "<< numcorrect << "/10000 were characterized correctly" << std::endl;
+        //std::cout << "At epoch end ff1:\n---------------\n" << ff1 << std::endl;
+#endif
     }
 
     costfile.close();
