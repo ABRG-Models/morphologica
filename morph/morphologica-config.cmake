adc1093ca3296fc1ccfe7dd83bfa03a5d401e2a8
Seb James <seb.james@sheffield.ac.uk>
2020-09-13 20:49:36 +0100

    Forgot to add this one.

---
+++ morph/morphologica-config.cmake
@@ -0,0 +1,3 @@
+# morphologica-config.cmake - package configuration file
+get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+include(${SELF_DIR}/${CMAKE_BUILD_TYPE}/morphologica.cmake)
