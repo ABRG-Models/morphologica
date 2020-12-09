989c6e80a166a00d1ab06585aed8a633da9d343d
Seb James <seb.james@sheffield.ac.uk>
2020-12-09 13:21:48 +0000

    A GradGenome test and a forgotten header

---
+++ morph/bn/GradGenosect.h
@@ -0,0 +1,43 @@
+#pragma once
+
+namespace morph {
+    namespace  bn {
+        //! template metafunction to select size type for a GradGenome's genosect_t
+        template <size_t N> struct GradGenosect {}; // Default should fail to compile
+        template<> struct GradGenosect<1> { typedef unsigned char type; };
+        template<> struct GradGenosect<2> { typedef unsigned char type; };
+        template<> struct GradGenosect<3> { typedef unsigned char type; };
+        template<> struct GradGenosect<4> { typedef unsigned char type; };
+
+        template<> struct GradGenosect<5> { typedef unsigned short type; };
+        template<> struct GradGenosect<6> { typedef unsigned short type; };
+        template<> struct GradGenosect<7> { typedef unsigned short type; };
+        template<> struct GradGenosect<8> { typedef unsigned short type; };
+
+        template<> struct GradGenosect<9>  { typedef unsigned int type; };
+        template<> struct GradGenosect<10> { typedef unsigned int type; };
+        template<> struct GradGenosect<11> { typedef unsigned int type; };
+        template<> struct GradGenosect<12> { typedef unsigned int type; };
+        template<> struct GradGenosect<13> { typedef unsigned int type; };
+        template<> struct GradGenosect<14> { typedef unsigned int type; };
+        template<> struct GradGenosect<15> { typedef unsigned int type; };
+        template<> struct GradGenosect<16> { typedef unsigned int type; };
+
+        template<> struct GradGenosect<17> { typedef unsigned long long int type; };
+        template<> struct GradGenosect<18> { typedef unsigned long long int type; };
+        template<> struct GradGenosect<19> { typedef unsigned long long int type; };
+        template<> struct GradGenosect<20> { typedef unsigned long long int type; };
+        template<> struct GradGenosect<21> { typedef unsigned long long int type; };
+        template<> struct GradGenosect<22> { typedef unsigned long long int type; };
+        template<> struct GradGenosect<23> { typedef unsigned long long int type; };
+        template<> struct GradGenosect<24> { typedef unsigned long long int type; };
+        template<> struct GradGenosect<25> { typedef unsigned long long int type; };
+        template<> struct GradGenosect<26> { typedef unsigned long long int type; };
+        template<> struct GradGenosect<27> { typedef unsigned long long int type; };
+        template<> struct GradGenosect<28> { typedef unsigned long long int type; };
+        template<> struct GradGenosect<29> { typedef unsigned long long int type; };
+        template<> struct GradGenosect<30> { typedef unsigned long long int type; };
+        template<> struct GradGenosect<31> { typedef unsigned long long int type; };
+        template<> struct GradGenosect<32> { typedef unsigned long long int type; };
+    }
+}
---
+++ tests/testGradGenome.cpp
@@ -0,0 +1,27 @@
+#include <morph/bn/GradGenome.h>
+
+using std::endl;
+using std::cout;
+
+// Note: compile time constants - i.e. not just const from early on in the program
+const size_t n = 2;
+
+// Globally initialise Random instance pointer - necessary for all progs using Genome
+template<> morph::bn::Random<n,n>* morph::bn::Random<n,n>::pInstance = 0;
+
+int main()
+{
+    morph::bn::GradGenome<n> gg;
+    gg.randomize();
+    cout << "Random gradient Genome:  " << gg << endl;
+
+    gg.set ("2-8");
+
+    cout << "For Gradient genome " << gg << ":\n"
+         << "Gene 0 climbs Gene 1? " << (gg.i_climbs_j (0,1) ? "true" : "false") << "\n"
+         << "Gene 0 descends Gene 1? " << (gg.i_descends_j (0,1) ? "true" : "false") << "\n"
+         << "Gene 1 climbs Gene 0? " << (gg.i_climbs_j (1,0) ? "true" : "false") << "\n"
+         << "Gene 1 descends Gene 0? " << (gg.i_descends_j (1,0) ? "true" : "false") << "\n";
+
+    return 0;
+}
