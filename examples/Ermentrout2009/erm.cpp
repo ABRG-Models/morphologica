943a6498c9a12ac8fe5c688e524515a2d2829a47
Seb James <seb.james@sheffield.ac.uk>
2021-01-09 14:08:36 +0000

    Adds Ermentrout example, which compiles as part of overall morphologica build process

---
+++ examples/Ermentrout2009/erm.cpp
@@ -0,0 +1,233 @@
+/*
+ * Implements 'Subbarrel Patterns in Somatosensory Cortical Barrels Can Emerge from
+ * Local Dynamic Instabilities', Ermentrout, Simons and Land, PLOS Comp Biol 2009
+ */
+
+/*!
+ * This will be passed as the template argument for RD_Plot and RD and
+ * should be defined when compiling.
+ */
+#ifndef FLT
+// Check CMakeLists.txt to change to double or float
+# error "Please define FLT when compiling (hint: See CMakeLists.txt)"
+#endif
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <chrono>
+
+// Morphologica headers: visualization and JSON config
+#include <morph/tools.h>
+#include <morph/Config.h>
+#include <morph/Scale.h>
+#include <morph/ColourMap.h>
+#include <morph/Visual.h>
+#include <morph/VisualDataModel.h>
+// Alias VisualDataModel<FLT>* as VdmPtr, to neaten code
+typedef morph::VisualDataModel<FLT>* VdmPtr;
+#include <morph/HexGridVisual.h>
+#include <morph/Vector.h>
+
+// Simulation code header
+#include "rd_erm.h"
+
+int main (int argc, char **argv)
+{
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " /path/to/params.json [/path/to/logdir]" << std::endl;
+        return 1;
+    }
+    std::string paramsfile (argv[1]);
+
+    // Get parameters from config file
+    morph::Config conf(paramsfile);
+    if (!conf.ready) {
+        std::cerr << "Failed to read config " << paramsfile << ". Exiting.\n";
+        return 1;
+    }
+
+    const unsigned int steps = conf.getUInt ("steps", 1000);
+    if (steps == 0) {
+        std::cerr << "Finished simulating 0 steps. Exiting.\n";
+        return 1;
+    }
+    const unsigned int logevery = conf.getUInt ("logevery", 100);
+    if (logevery == 0) {
+        std::cerr << "Can't log every 0 steps. Exiting.\n";
+        return 1;
+    }
+    bool overwrite_logs = conf.getBool ("overwrite_logs", false);
+    std::string logpath = conf.getString ("logpath", "logs/erm2");
+    if (argc == 3) {
+        std::string argpath(argv[2]);
+        std::cerr << "Overriding the config-given logpath " << logpath << " with " << argpath << std::endl;
+        logpath = argpath;
+        if (overwrite_logs == true) {
+            std::cerr << "WARNING: You set a command line log path.\n"
+                      << "       : Note that the parameters config permits the program to OVERWRITE LOG\n"
+                      << "       : FILES on each run (\"overwrite_logs\" is set to true).\n";
+        }
+    }
+
+    std::cout << "steps to simulate: " << steps << std::endl;
+
+    const unsigned int plotevery = conf.getUInt ("plotevery", 10);
+    const bool vidframes = conf.getBool ("vidframes", false);
+    unsigned int framecount = 0;
+
+    const unsigned int win_width = conf.getUInt ("win_width", 340);
+    unsigned int win_height = static_cast<unsigned int>(0.8824f * (float)win_width);
+
+    // Instantiate the model object
+    RD_Erm<FLT> RD;
+
+    RD.svgpath = conf.getString ("svgpath", "./ellipse.svg");
+    RD.logpath = logpath;
+
+    RD.set_dt(static_cast<FLT>(conf.getDouble ("dt", 0.00001)));
+    RD.hextohex_d = conf.getFloat ("hextohex_d", 0.01f);
+    RD.boundaryFalloffDist = conf.getFloat ("boundaryFalloffDist", 0.01f);
+;
+
+    RD.N = 1;
+    RD.Dn = conf.getDouble ("Dn", 0.3);
+    RD.Dc = conf.getDouble ("Dc", 0.3*0.3); // 0.3 * Dn
+    RD.beta = conf.getDouble ("beta", 5.0);
+    RD.a = conf.getDouble ("a", 1.0);
+    RD.b = conf.getDouble ("b", 1.0);
+    RD.mu = conf.getDouble ("mu", 1.0);
+    // Set chi to Dn, as in the paper (see linear analysis)
+    RD.chi = RD.Dn; // or: conf.getDouble ("chi", 0.3);
+
+    // Allocate and initialise the model
+    RD.allocate();
+    RD.init();
+
+    // Now create a log directory if necessary, and exit on any failures.
+    if (morph::Tools::dirExists (logpath) == false) {
+        morph::Tools::createDir (logpath);
+        if (morph::Tools::dirExists (logpath) == false) {
+            std::cerr << "Failed to create the logpath directory "
+                      << logpath << " which does not exist.\n";
+            return 1;
+        }
+    } else {
+        // Directory DOES exist. See if it contains a previous run and
+        // exit without overwriting to avoid confusion.
+        if (overwrite_logs == false
+            && (morph::Tools::fileExists (logpath + "/params.json") == true
+                || morph::Tools::fileExists (logpath + "/positions.h5") == true)) {
+            std::cerr << "Seems like a previous simulation was logged in " << logpath << ".\n"
+                      << "Please clean it out manually, choose another directory or set\n"
+                      << "overwrite_logs to true in your parameters config JSON file.\n";
+            return 1;
+        }
+    }
+
+    // As RD.allocate() as been called (and log directory has been
+    // created/verified ready), positions can be saved to file.
+    RD.savePositions();
+
+    // Set up the morph::Visual object
+    morph::Visual plt (win_width, win_height, "Ermentrout sub-barrels");
+    plt.zNear = 0.001;
+    plt.zFar = 50;
+    plt.fov = 45;
+    plt.setZDefault (-10.0f);
+    plt.showCoordArrows = true;
+    plt.showTitle = false;
+    // You can lock movement of the scene
+    plt.sceneLocked = conf.getBool ("sceneLocked", false);
+    // You can set the default scene x/y/z offsets
+    plt.setZDefault (conf.getFloat ("z_default", -10.0f));
+    plt.setSceneTransXY (conf.getFloat ("x_default", 0.0f),
+                         conf.getFloat ("y_default", 0.0f));
+    // Make this larger to "scroll in and out of the image" faster
+    plt.scenetrans_stepsize = 0.5;
+
+    // Add a morph::HexGridVisual to the morph::Visual.
+    morph::Vector<float, 3> spatOff = {0,0,0}; // spatial offset
+    // Data scaling parameters
+    float _m = 0.2;
+    float _c = 0.0;
+    // Z position scaling - how hilly/bumpy the visual will be.
+    morph::Scale<FLT> zscale; zscale.setParams (_m/10.0f, _c/10.0f);
+    // The second is the colour scaling.
+    morph::Scale<FLT> cscale; cscale.setParams (_m, _c);
+
+    // The n variable
+    unsigned int side = static_cast<unsigned int>(floor (sqrt (RD.N)));
+    // FIXME: I hate this big constructor. Figure out what's necessary and set the other
+    // stuff as after-the-instantiation setters.
+    morph::HexGridVisual<FLT>* hgv1 = new morph::HexGridVisual<FLT> (plt.shaderprog,
+                                                                     plt.tshaderprog,
+                                                                     RD.hg,
+                                                                     spatOff,
+                                                                     &RD.n[0],
+                                                                     zscale,
+                                                                     cscale,
+                                                                     morph::ColourMapType::Plasma);
+    unsigned int n_idx = plt.addVisualModel (hgv1);
+
+    // Set up the render clock
+    std::chrono::steady_clock::time_point lastrender = std::chrono::steady_clock::now();
+
+    // Start the loop
+    bool doing = true;
+    while (doing) {
+        // Step the model:
+        RD.step();
+
+        if ((RD.stepCount % plotevery) == 0) {
+            // Plot n and c
+            static_cast<VdmPtr>(plt.getVisualModel(n_idx))->updateData (&RD.n[0]);
+            // add c
+        }
+
+         // Save data every 'logevery' steps
+        if ((RD.stepCount % logevery) == 0) {
+            std::cout << "Logging data at step " << RD.stepCount << std::endl;
+            RD.saveState();
+        }
+
+        // After a while, stop:
+        if (RD.stepCount > steps) { doing = false; }
+
+        // rendering the graphics.
+        std::chrono::steady_clock::duration sincerender = std::chrono::steady_clock::now() - lastrender;
+        if (std::chrono::duration_cast<std::chrono::milliseconds>(sincerender).count() > 17) { // 17 is about 60 Hz
+            glfwPollEvents();
+            plt.render();
+            lastrender = std::chrono::steady_clock::now();
+        }
+    }
+
+    // Before exit, save data
+    RD.saveState();
+
+    // Add simulation runtime information to the config, before saving it out as params.json
+    conf.set ("float_width", (unsigned int)sizeof(FLT));
+    std::string tnow = morph::Tools::timeNow();
+    conf.set ("sim_ran_at_time", tnow.substr(0,tnow.size()-1));
+    conf.set ("final_step", RD.stepCount);
+    conf.set ("hextohex_d", RD.hextohex_d);
+    conf.set ("dt", RD.get_dt());
+#ifndef __OSX__ // Currently Config::insertGitInfo fails on Macs
+    conf.insertGitInfo ("sim/");
+#endif
+    // Store the binary name and command argument into root, too.
+    if (argc > 0) { conf.set("argv0", argv[0]); }
+    if (argc > 1) { conf.set("argv1", argv[1]); }
+    const std::string paramsCopy = logpath + "/params.json";
+    conf.write (paramsCopy);
+    if (conf.ready == false) {
+        std::cerr << "Warning: Something went wrong writing a copy of the params.json: "
+                  << conf.emsg << std::endl;
+    }
+
+    std::cout << "Press x in graphics window to exit.\n";
+    plt.keepOpen();
+
+    return 0;
+};
---
+++ examples/Ermentrout2009/rd_erm.h
@@ -0,0 +1,260 @@
+/*
+ * 2D Ermentrout system deriving from RD_Base.
+ */
+#include <morph/tools.h>
+#include <morph/HexGrid.h>
+#include <morph/ReadCurves.h>
+#include <morph/HdfData.h>
+#include <morph/RD_Base.h>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <iomanip>
+#include <cmath>
+#include <hdf5.h>
+#include <unistd.h>
+
+/*
+ * Macros for testing neighbours. The step along for neighbours on the
+ * rows above/below is given by:
+ *
+ * Dest  | step
+ * ----------------------
+ * NNE   | +rowlen
+ * NNW   | +rowlen - 1
+ * NSW   | -rowlen
+ * NSE   | -rowlen + 1
+ */
+#define NE(hi) (this->hg->d_ne[hi])
+#define HAS_NE(hi) (this->hg->d_ne[hi] == -1 ? false : true)
+
+#define NW(hi) (this->hg->d_nw[hi])
+#define HAS_NW(hi) (this->hg->d_nw[hi] == -1 ? false : true)
+
+#define NNE(hi) (this->hg->d_nne[hi])
+#define HAS_NNE(hi) (this->hg->d_nne[hi] == -1 ? false : true)
+
+#define NNW(hi) (this->hg->d_nnw[hi])
+#define HAS_NNW(hi) (this->hg->d_nnw[hi] == -1 ? false : true)
+
+#define NSE(hi) (this->hg->d_nse[hi])
+#define HAS_NSE(hi) (this->hg->d_nse[hi] == -1 ? false : true)
+
+#define NSW(hi) (this->hg->d_nsw[hi])
+#define HAS_NSW(hi) (this->hg->d_nsw[hi] == -1 ? false : true)
+
+#define IF_HAS_NE(hi, yesval, noval)  (HAS_NE(hi)  ? yesval : noval)
+#define IF_HAS_NNE(hi, yesval, noval) (HAS_NNE(hi) ? yesval : noval)
+#define IF_HAS_NNW(hi, yesval, noval) (HAS_NNW(hi) ? yesval : noval)
+#define IF_HAS_NW(hi, yesval, noval)  (HAS_NW(hi)  ? yesval : noval)
+#define IF_HAS_NSW(hi, yesval, noval) (HAS_NSW(hi) ? yesval : noval)
+#define IF_HAS_NSE(hi, yesval, noval) (HAS_NSE(hi) ? yesval : noval)
+
+/*!
+ * Reaction diffusion system; Ermentrout 2009.
+ */
+template <class Flt>
+class RD_Erm : public morph::RD_Base<Flt>
+{
+public:
+    //! Set N>1 for maintaining multiple expression gradients
+    alignas(Flt) unsigned int N = 1;
+    //! The c_i(x,t) variables from the Ermentrout paper (chemoattractant concentration)
+    alignas(Flt) std::vector<std::vector<Flt> > c;
+    //! The n_i(x,t) variables from the Ermentrout paper (density of tc axons)
+    alignas(Flt) std::vector<std::vector<Flt> > n;
+    //! Holds the Laplacian
+    alignas(Flt) std::vector<std::vector<Flt> > lapl;
+    //! Holds the Poisson terms (final non-linear term in Ermentrout equation 1)
+    alignas(Flt) std::vector<std::vector<Flt> > poiss;
+
+    // Parameters of the Ermentrout model - default values.
+    //! Diffusion constant for n
+    alignas(Flt) Flt Dn = 0.3;
+    //! Diffusion constant for c
+    alignas(Flt) Flt Dc = Dn * 0.3;
+    //! saturation term in function for production of c
+    alignas(Flt) Flt beta = 5.0;
+    //! production of new axon branches
+    alignas(Flt) Flt a = 1.0;
+    //! pruning constant
+    alignas(Flt) Flt b = 1.0;
+    //! decay of chemoattractant constant
+    alignas(Flt) Flt mu = 1.0;
+    //! degree of attraction of chemoattractant
+    alignas(Flt) Flt chi = Dn;
+
+    //! Frame number, used when saving PNG movie frames.
+    unsigned int frameN = 0;
+
+    //! Simple constructor; no arguments.
+    RD_Erm (void) : morph::RD_Base<Flt>() {}
+
+    //! Perform memory allocations, vector resizes and so on.
+    virtual void allocate (void) { morph::RD_Base<Flt>::allocate(); }
+
+    //! Initialise HexGrid, variables. Carry out any one-time computations of the model.
+    virtual void init (void)
+    {
+        // Resize and zero-initialise the various containers
+        this->resize_vector_vector (this->c, this->N);
+        this->resize_vector_vector (this->n, this->N);
+        this->resize_vector_vector (this->lapl, this->N);
+        this->resize_vector_vector (this->poiss, this->N);
+
+        // Initialise with noise
+        for (unsigned int i  = 0; i < this->N; ++i) {
+            this->noiseify_vector_variable (this->n[i], 1., 0.01);
+            this->noiseify_vector_variable (this->c[i], beta*0.5, 0.01);
+        }
+    }
+
+    //! Compute one step of the model
+    virtual void step (void)
+    {
+        this->stepCount++;
+
+        for (unsigned int i=0; i<this->N; ++i) {
+
+            this->compute_poiss (n[i],c[i],i);  // compute the non-linear Poission term in Eq1
+            this->compute_lapl (n[i], i);       // populate lapl[i] with laplacian of n
+
+            // integrate n
+            for (unsigned int h=0; h<this->nhex; ++h) {
+                n[i][h] += (a - b*n[i][h] + Dn*lapl[i][h] - chi*poiss[i][h])*this->dt;
+            }
+
+            this->compute_lapl (c[i], i);       // populate lapl[i] with laplacian of c
+
+            // integrate c
+            Flt n2;
+            for (unsigned int h=0; h<this->nhex; ++h) {
+                n2 = n[i][h]*n[i][h];
+                c[i][h] += (beta*n2/(1.+n2) - mu*c[i][h] +Dc*lapl[i][h])*this->dt;
+            }
+        }
+    }
+
+    //! Computes the Laplacian Stable with dt = 0.0001;
+    void compute_lapl (std::vector<Flt>& fa, unsigned int i)
+    {
+#pragma omp parallel for schedule(static)
+        for (unsigned int hi=0; hi<this->nhex; ++hi) {
+
+            // 1. The D Del^2 term
+
+            // Compute the sum around the neighbours
+            Flt thesum = -6 * fa[hi];
+            if (HAS_NE(hi)) {
+                thesum += fa[NE(hi)];
+            } else {
+                thesum += fa[hi]; // A ghost neighbour-east with same value as Hex_0
+            }
+            if (HAS_NNE(hi)) {
+                thesum += fa[NNE(hi)];
+            } else {
+                thesum += fa[hi];
+            }
+            if (HAS_NNW(hi)) {
+                thesum += fa[NNW(hi)];
+            } else {
+                thesum += fa[hi];
+            }
+            if (HAS_NW(hi)) {
+                thesum += fa[NW(hi)];
+            } else {
+                thesum += fa[hi];
+            }
+            if (HAS_NSW(hi)) {
+                thesum += fa[NSW(hi)];
+            } else {
+                thesum += fa[hi];
+            }
+            if (HAS_NSE(hi)) {
+                thesum += fa[NSE(hi)];
+            } else {
+                thesum += fa[hi];
+            }
+
+            this->lapl[i][hi] = this->twoover3dd * thesum;
+        }
+    }
+
+    //! Computes the Poisson term. Stable with dt = 0.0001;
+    void compute_poiss (std::vector<Flt>& fa1, std::vector<Flt>& fa2, unsigned int i)
+    {
+        // Compute non-linear term
+#pragma omp parallel for schedule(static)
+        for (unsigned int hi=0; hi<this->nhex; ++hi) {
+
+            std::vector<Flt> dum1(6,fa1[hi]);
+            std::vector<Flt> dum2(6,fa2[hi]);
+
+            if (HAS_NE(hi)) {
+                dum1[0] = fa1[NE(hi)];
+                dum2[0] = fa2[NE(hi)];
+            }
+            if (HAS_NNE(hi)) {
+                dum1[1] = fa1[NNE(hi)];
+                dum2[1] = fa2[NNE(hi)];
+            }
+            if (HAS_NNW(hi)) {
+                dum1[2] = fa1[NNW(hi)];
+                dum2[2] = fa2[NNW(hi)];
+            }
+            if (HAS_NW(hi)) {
+                dum1[3] = fa1[NW(hi)];
+                dum2[3] = fa2[NW(hi)];
+            }
+            if (HAS_NSW(hi)) {
+                dum1[4] = fa1[NSW(hi)];
+                dum2[4] = fa2[NSW(hi)];
+            }
+            if (HAS_NSE(hi)) {
+                dum1[5] = fa1[NSE(hi)];
+                dum2[5] = fa2[NSE(hi)];
+            }
+
+            // John Brooke's final thesis solution (based on 'finite volume method'
+            // of Lee et al. https://doi.org/10.1080/00207160.2013.864392
+            Flt val = (dum1[0]+fa1[hi]) * (dum2[0]-fa2[hi])
+                + (dum1[1]+fa1[hi]) * (dum2[1]-fa2[hi])
+                + (dum1[2]+fa1[hi]) * (dum2[2]-fa2[hi])
+                + (dum1[3]+fa1[hi]) * (dum2[3]-fa2[hi])
+                + (dum1[4]+fa1[hi]) * (dum2[4]-fa2[hi])
+                + (dum1[5]+fa1[hi]) * (dum2[5]-fa2[hi]);
+            this->poiss[i][hi] = val * this->oneover3dd;// / (3 * this->d * this->d);
+        }
+    }
+
+    //! Save state to HDF5
+    void saveState (void)
+    {
+        std::string fname = this->logpath + "/2Derm.h5";
+        morph::HdfData data (fname);
+
+        // Save some variables
+        for (unsigned int i = 0; i<this->N; ++i) {
+
+            std::stringstream vss;
+            vss << "c_" << i;
+            std::string vname = vss.str();
+            data.add_contained_vals (vname.c_str(), this->c[i]);
+            vname[0] = 'n';
+            data.add_contained_vals (vname.c_str(), this->n[i]);
+        }
+
+        // Parameters
+        data.add_val ("/Dn", this->Dn);
+        data.add_val ("/Dc", this->Dc);
+        data.add_val ("/beta", this->beta);
+        data.add_val ("/a", this->a);
+        data.add_val ("/b", this->b);
+        data.add_val ("/mu", this->mu);
+        data.add_val ("/chi", this->chi);
+
+        // HexGrid information
+        this->saveHexPositions (data);
+    }
+
+}; // RD_Erm
