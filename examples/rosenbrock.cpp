052a31d6ab32b4f9381696f1420ce5713da02aa6
Seb James <seb.james@sheffield.ac.uk>
2021-02-04 19:40:40 +0000

    Sorts out the visuals and colourmap

--- examples/rosenbrock.cpp
+++ examples/rosenbrock.cpp
@@ -8,8 +8,10 @@
 #include <morph/TriFrameVisual.h>
 #include <morph/HexGrid.h>
 #include <morph/HexGridVisual.h>
+#include <morph/MathAlgo.h>
 #include <iostream>
 #include <unistd.h>
+#include <chrono>
 
 // Here's the Rosenbrock banana function
 FLT banana (FLT x, FLT y) {
@@ -28,6 +30,7 @@ int main()
     // Set up a visual environment
     morph::Visual v(2600, 1800, "Rosenbrock bananas", {-0.8,-0.8}, {.05,.05,.05}, 2.0f, 0.01f);
     v.zNear = 0.001;
+    v.zFar = 100000;
     v.fov=60;
     v.showCoordArrows = true;
     v.lightingEffects (true);
@@ -45,7 +48,7 @@ int main()
     morph::Vector<float> _offset = {0,0,0};
     morph::TriFrameVisual<FLT>* tfv = new morph::TriFrameVisual<FLT>(v.shaderprog, _offset);
     tfv->radius = 0.01f;
-    tfv->sradius = 0.02f;
+    tfv->sradius = 0.01f;
     vector<FLT> tri_values(3, 0);
     vector<morph::Vector<float>> tri_coords(3);
     tri_coords[0] = { v1[0], v1[1], 0.0 };
@@ -62,17 +65,20 @@ int main()
     std::cout << "test point on banana function = " << test << " (should be 0).\n";
 
     // Evaluate banana function and plot
-    morph::HexGrid hg (0.1, 15, 0, morph::HexDomainShape::Boundary);
-    hg.setCircularBoundary (4);
+    morph::HexGrid hg (0.01, 10, 0, morph::HexDomainShape::Boundary);
+    hg.setCircularBoundary (2.5);
     vector<FLT> banana_vals(hg.num(), 0.0f);
     for (size_t i = 0; i < hg.num(); ++i) {
         banana_vals[i] = banana (hg.d_x[i], hg.d_y[i]);
     }
+    std::pair<FLT, FLT> mm = morph::MathAlgo::maxmin(banana_vals);
+    std::cout << "Banana surface max/min: " << mm.first << "," << mm.second << std::endl;
     morph::HexGridVisual<FLT>* hgv = new morph::HexGridVisual<FLT>(v.shaderprog, v.tshaderprog, &hg, _offset);
+    hgv->hexVisMode = morph::HexVisMode::Triangles;
     hgv->cm.setType (morph::ColourMapType::Viridis);
     hgv->setScalarData (&banana_vals);
     hgv->zScale.setParams (0.001f, 0.0f);
-    hgv->colourScale.compute_autoscale (0.0f, 0.1f);
+    hgv->colourScale.compute_autoscale (0.01f, 5.0f);
     hgv->setAlpha (0.4f);
     hgv->finalize();
     v.addVisualModel (hgv);
@@ -85,55 +91,69 @@ int main()
     // Temporary variable
     FLT val;
 
+    // if using plotting, then set up the render clock
+    std::chrono::steady_clock::time_point lastrender = std::chrono::steady_clock::now();
+    std::chrono::steady_clock::time_point lastoptstep = std::chrono::steady_clock::now();
+
     // Now do the business
     unsigned int lcount = 0;
     while (simp.state != morph::NM_Simplex_State::ReadyToStop) {
-        lcount++;
-        if (simp.state == morph::NM_Simplex_State::NeedToComputeThenOrder) {
-            // 1. apply objective to each vertex
-            for (unsigned int i = 0; i <= simp.n; ++i) {
-                simp.values[i] = banana (simp.vertices[i][0], simp.vertices[i][1]);
+
+        // Perform optimisation steps slowly
+        std::chrono::steady_clock::duration sinceoptstep = std::chrono::steady_clock::now() - lastoptstep;
+        if (std::chrono::duration_cast<std::chrono::milliseconds>(sinceoptstep).count() > 250) {
+            lcount++;
+            if (simp.state == morph::NM_Simplex_State::NeedToComputeThenOrder) {
+                // 1. apply objective to each vertex
+                for (unsigned int i = 0; i <= simp.n; ++i) {
+                    simp.values[i] = banana (simp.vertices[i][0], simp.vertices[i][1]);
+                }
+                simp.order();
+
+            } else if (simp.state == morph::NM_Simplex_State::NeedToOrder) {
+                simp.order();
+
+            } else if (simp.state == morph::NM_Simplex_State::NeedToComputeReflection) {
+                val = banana (simp.xr[0], simp.xr[1]);
+                simp.apply_reflection (val);
+
+            } else if (simp.state == morph::NM_Simplex_State::NeedToComputeExpansion) {
+                val = banana (simp.xe[0], simp.xe[1]);
+                simp.apply_expansion (val);
+
+            } else if (simp.state == morph::NM_Simplex_State::NeedToComputeContraction) {
+                val = banana (simp.xc[0], simp.xc[1]);
+                simp.apply_contraction (val);
             }
-            simp.order();
 
-        } else if (simp.state == morph::NM_Simplex_State::NeedToOrder) {
-            simp.order();
-
-        } else if (simp.state == morph::NM_Simplex_State::NeedToComputeReflection) {
-            val = banana (simp.xr[0], simp.xr[1]);
-            simp.apply_reflection (val);
-
-        } else if (simp.state == morph::NM_Simplex_State::NeedToComputeExpansion) {
-            val = banana (simp.xe[0], simp.xe[1]);
-            simp.apply_expansion (val);
-
-        } else if (simp.state == morph::NM_Simplex_State::NeedToComputeContraction) {
-            val = banana (simp.xc[0], simp.xc[1]);
-            simp.apply_contraction (val);
-        }
-
-#if 1
-        // Output in matlab/octave format to plot3() the simplex.
-        cout << "simp=[";
-        for (unsigned int i = 0; i <= simp.n; ++i) {
-            cout << simp.vertices[i][0] << "," << simp.vertices[i][1] << ",val" << simp.values[i] << ";";
-        }
-        cout << simp.vertices[0][0] << "," << simp.vertices[0][1] << ",val" << simp.values[0] << "];" << endl;
-        //cout << "order:" << simp.vertex_order[0] << ","<< simp.vertex_order[1] << ","<< simp.vertex_order[2] << endl;
+#if 0
+            // Output in matlab/octave format to plot3() the simplex.
+            cout << "simp=[";
+            for (unsigned int i = 0; i <= simp.n; ++i) {
+                cout << simp.vertices[i][0] << "," << simp.vertices[i][1] << ",val" << simp.values[i] << ";";
+            }
+            cout << simp.vertices[0][0] << "," << simp.vertices[0][1] << ",val" << simp.values[0] << "];" << endl;
+            //cout << "order:" << simp.vertex_order[0] << ","<< simp.vertex_order[1] << ","<< simp.vertex_order[2] << endl;
 #endif
 
-        // Visualise the triangle defined by simp.vertices
+            // Visualise the triangle defined by simp.vertices
 
-        // Copy data out from NM_Simplex
-        for (unsigned int i = 0; i <= simp.n; ++i) {
-            tri_coords[i] = { simp.vertices[i][0], simp.vertices[i][1], 0.0 };
-            tri_values[i] = simp.values[i];
+            // Copy data out from NM_Simplex
+            for (unsigned int i = 0; i <= simp.n; ++i) {
+                tri_coords[i] = { simp.vertices[i][0], simp.vertices[i][1], 0.0 };
+                tri_values[i] = simp.values[i];
+            }
+            tfv->reinit();
+
+            lastoptstep = std::chrono::steady_clock::now();
         }
-        tfv->reinit();
 
-        v.render();
-
-        usleep (400000);
+        std::chrono::steady_clock::duration sincerender = std::chrono::steady_clock::now() - lastrender;
+        if (std::chrono::duration_cast<std::chrono::milliseconds>(sincerender).count() > 17) { // 17 is about 60 Hz
+            glfwPollEvents();
+            v.render();
+            lastrender = std::chrono::steady_clock::now();
+        }
     }
     vector<FLT> thebest = simp.best_vertex();
     FLT bestval = simp.best_value();
