ee7bb6f907a7d6a2f3a1130fb257204e9db1b5fd
Seb James <seb.james@sheffield.ac.uk>
2021-03-11 19:27:06 +0000

    Convert tabs to 4 spaces

--- softmats/src/collisions/chashtable.h
+++ softmats/src/collisions/chashtable.h
@@ -1,5 +1,5 @@
 #ifndef CHASHTABLE_H
-#define CHASHTABLE_H 
+#define CHASHTABLE_H
 
 #include <map>
 #include <list>
@@ -14,31 +14,31 @@ using namespace arma;
 namespace morph{ namespace softmats{
 
 typedef struct {
-	list<int> items;
-	int timestamp;
+    list<int> items;
+    int timestamp;
 } CHashItem;
 
 /**
-CHashTable - Implements a hash table for the search of collisions 
-			 in a given volume.
+CHashTable - Implements a hash table for the search of collisions
+             in a given volume.
 */
 class CHashTable{
 
-	const unsigned long p1 = 73856093;
-	const unsigned long p2 = 19349663;
-	const unsigned long p3 = 83492791;
+    const unsigned long p1 = 73856093;
+    const unsigned long p2 = 19349663;
+    const unsigned long p3 = 83492791;
 public:
-	int n;
-	double l; // Cell size
-	CHashTable( int n, double l );
-	vector<CHashItem> hashes;
+    int n;
+    double l; // Cell size
+    CHashTable( int n, double l );
+    vector<CHashItem> hashes;
 
-	int discretize( double a );
-	void discretizeBox( Box *b );
-	unsigned int getHash( vec point );
-	unsigned int getHashDiscrete( vec p );
-	void hashIn( vec point, int index, int step ); 
-	CHashItem getItem( unsigned int h );
+    int discretize( double a );
+    void discretizeBox( Box *b );
+    unsigned int getHash( vec point );
+    unsigned int getHashDiscrete( vec p );
+    void hashIn( vec point, int index, int step );
+    CHashItem getItem( unsigned int h );
 
 };
 
