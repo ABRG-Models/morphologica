2eed3c9d8d1d657def4444b3868d4af89d0d1a0f
Seb James <seb.james@sheffield.ac.uk>
2021-06-23 16:14:16 +0100

    A histogram class

---
+++ morph/histo.h
@@ -0,0 +1,58 @@
+/*
+ * A histogram class
+ */
+#pragma once
+
+#include <morph/vVector.h>
+#include <morph/MathAlgo.h>
+#include <memory>
+#include <utility>
+
+namespace morph {
+
+    template <typename T=float>
+    struct histo
+    {
+        template < template <typename, typename> typename Container,
+                   typename Allocator=std::allocator<T> >
+        histo (const Container<T, Allocator>& data, size_t n)
+        {
+            this->bins.resize(n, T{0});
+            this->binedges.resize(n+1, T{0});
+            this->counts.resize(n, 0);
+            this->proportions.resize(n, T{0});
+            this->datacount = static_cast<T>(data.size());
+
+            // Compute bin widths from range of data and n.
+            std::pair<T,T> maximini = morph::MathAlgo::maxmin (data);
+            this->max = maximini.first;
+            this->min = maximini.second;
+            this->range = max - min;
+            this->binwidth = this->range / n;
+            for (size_t i = 0; i < n; ++i) {
+                // bins[i] = min + i*bw + bw/2 but do the additions after the loop
+                this->bins[i] = i * this->binwidth;
+                this->binedges[i+1] = (i+1) * this->binwidth;
+            }
+            this->bins += (this->min + (this->binwidth/T{2}));
+            this->binedges += this->min;
+
+            // Compute counts
+            for (auto datum : data) {
+                size_t idx = std::floor(((datum - this->min)/this->range)*n);
+                counts[idx] += T{1};
+            }
+            this->proportions = counts/this->datacount;
+        }
+
+        T min = T{0}; // min value in data
+        T max = T{0}; // max value in data
+        T range = T{0}; // range of data
+        T datacount = T{0}; // how many elements were there in data?
+        T binwidth = T{0}; // Width of each bin
+        morph::vVector<T> bins; // centres of bins
+        morph::vVector<T> binedges;
+        morph::vVector<T> counts;
+        morph::vVector<T> proportions;
+    };
+}
