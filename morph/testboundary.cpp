5403fa0d1321bbf38420364dc144932721f2c8e1
Seb James <seb.james@sheffield.ac.uk>
2021-08-26 13:22:34 +0100

    Reinstating this utility prog, but in the examples directory

--- morph/testboundary.cpp
+++ morph/testboundary.cpp
@@ -25,7 +25,8 @@
 #include <math.h>
 #include <morph/BezCoord.h>
 
-#include <morph/display.h>
+#include <morph/Visual.h>
+#include <morph/HexGridVisual.h>
 
 using namespace std;
 using morph::ReadCurves;
@@ -71,6 +72,7 @@ int main(int argc, char** argv)
              << r.getScale_svgpermm() << " units/mm" << endl;
         cout << "Number of hexes within the boundary: " << hg.num() << endl;
 
+#if 0
         vector<double> fix(3, 0.0);
         vector<double> eye(3, 0.0);
         vector<double> rot(3, 0.0);
@@ -123,6 +125,43 @@ int main(int argc, char** argv)
         cin >> a;
 
         disp.closeDisplay();
+#else
+        // Do it with morph::Visual
+        morph::Visual v(1600, 1000, "Your SVG defined boundary");
+        v.lightingEffects();
+        morph::Vector<float, 3> offset = { 0.0f, -0.0f, 0.0f };
+        morph::HexGridVisual<float>* hgv = new morph::HexGridVisual<float>(v.shaderprog, v.tshaderprog, &hg, offset);
+        // Set up data for the HexGridVisual and colour hexes according to their state as being boundary/inside/domain, etc
+        vector<float> colours (hg.size(), 0.0f);
+        static constexpr float cl_boundary_and_in = 0.9f;
+        static constexpr float cl_bndryonly = 0.8f;
+        static constexpr float cl_domain = 0.5f;
+        static constexpr float cl_inside = 0.15f;
+        for (auto h : hg.hexen) {
+            if (h.boundaryHex() && h.insideBoundary()) {
+                // red is boundary hex AND inside boundary
+                colours[h.vi] = cl_boundary_and_in;
+            } else if (h.boundaryHex()) {
+                // orange is boundary ONLY
+                colours[h.vi] = cl_bndryonly;
+            } else if (h.insideBoundary()) {
+                // Inside boundary -  blue
+                colours[h.vi] = cl_inside;
+            } else {
+                // The domain - greenish
+                colours[h.vi] = cl_domain;
+            }
+        }
+        hgv->cm.setType (morph::ColourMapType::Jet);
+        hgv->setScalarData (&colours);
+        hgv->hexVisMode = morph::HexVisMode::HexInterp; // Or morph::HexVisMode::Triangles for a smoother surface plot
+        hgv->finalize();
+        v.addVisualModel (hgv);
+        while (v.readyToFinish == false) {
+            glfwWaitEventsTimeout (0.018);
+            v.render();
+        }
+#endif
 
     } catch (const exception& e) {
         cerr << "Caught exception reading " << argv[1] << ": " << e.what() << endl;
