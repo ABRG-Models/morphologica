9c71a0765b34b5a13906f101c59f6fc5e465e2a2
Seb James <seb.james@sheffield.ac.uk>
2021-09-22 10:03:57 +0100

    make graph1 example really nice

--- examples/graph1.cpp
+++ examples/graph1.cpp
@@ -9,32 +9,21 @@
 int main (int argc, char** argv)
 {
     // Set up a morph::Visual 'scene environment'.
-    morph::Visual v(1024, 768, "Made with morph::GraphVisual", {-0.8,-0.8}, {.1,.1,.1}, 1.0f, 0.01f);
-    v.backgroundWhite();
-
+    morph::Visual v(1024, 768, "Made with morph::GraphVisual");
     // Create a new GraphVisual with offset within the scene of 0,0,0
     morph::GraphVisual<float>* gv = new morph::GraphVisual<float> (v.shaderprog, v.tshaderprog, {0,0,0});
-
-    // Create some data (y = x^3):
+    // Create some data for the x axis:
     morph::vVector<float> x;
+    // This works like numpy's linspace() (the 3 args are "start", "end" and "num"):
     x.linspace (-0.5, 0.8, 14);
-    morph::vVector<float> y = x.pow(3);
-
-    // Set the data into the graph
-    gv->setdata (x, y);
-
+    // Set a graph up of y = x^3
+    gv->setdata (x, x.pow(3));
     // Complete the setup
     gv->finalize();
-
     // Add the GraphVisual to the Visual scene
-    v.addVisualModel (static_cast<morph::VisualModel*>(gv));
-
-    // Render the scene on the screen
-    v.render();
-    while (v.readyToFinish == false) {
-        glfwWaitEventsTimeout (0.018);
-        v.render();
-    }
+    v.addVisualModel (gv);
+    // Render the scene on the screen until user quits with 'x'
+    v.keepOpen();
 
     return 0;
 }
