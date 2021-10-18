771a9dd7a853405a5e407908d5863c59a2e0c3b0
Seb James <seb.james@sheffield.ac.uk>
2021-10-18 08:52:40 +0100

    Adds a templated mathematical constant scheme, which I prefer to the previous one (which is left in place)

--- morph/MathConst.h
+++ morph/MathConst.h
@@ -28,4 +28,32 @@ namespace morph {
     //! 2*PI
     const float  TWO_PI_F = 6.283185307179586476925286766559005768394338798750212f;
     const double TWO_PI_D = 6.283185307179586476925286766559005768394338798750212;
+
+    //! Templated mathematical constants.
+    //! Usage example: morph::mathconst<float>::pi_over_2
+    //! T defaults to double, so morph::mathconst::pi gives pi in double precision.
+    template <typename T=double>
+    struct mathconst
+    {
+        static constexpr T sqrt_of_3 = T{1.732050807568877293527446341505872366942805253810381};
+        static constexpr T sqrt_of_3_over_2 = T{0.86602540378443864676372317075293618347140262690519};
+        static constexpr T pi = T{3.141592653589793238462643383279502884197169399375106};
+        static constexpr T pi_over_2 = pi/T{2};
+        static constexpr T pi_over_3 = pi/T{3};
+        static constexpr T pi_over_4 = pi/T{4};
+        static constexpr T pi_over_5 = pi/T{5};
+        static constexpr T pi_over_6 = pi/T{6};
+        static constexpr T pi_over_7 = pi/T{7};
+        static constexpr T pi_over_8 = pi/T{8};
+        static constexpr T pi_over_9 = pi/T{9};
+        static constexpr T two_pi   = pi*T{2};
+        static constexpr T three_pi = pi*T{3};
+        static constexpr T four_pi  = pi*T{4};
+        static constexpr T five_pi  = pi*T{5};
+        static constexpr T six_pi   = pi*T{6};
+        static constexpr T seven_pi = pi*T{7};
+        static constexpr T eight_pi = pi*T{8};
+        static constexpr T nine_pi  = pi*T{9};
+        static constexpr T three_pi_over_2 = pi*T{3}/T{2};
+    };
 }
