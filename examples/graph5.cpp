763e9aad23601b7795e8ac41ea51db78f8f9eac8
Seb James <seb.james@sheffield.ac.uk>
2022-01-29 08:54:50 +0000

    Sometimes it's worth adding a using directive

--- examples/graph5.cpp
+++ examples/graph5.cpp
@@ -3,6 +3,8 @@
 #include <morph/GraphVisual.h>
 #include <morph/vVector.h>
 
+using morph::unicode;
+
 int main()
 {
     // Set up a morph::Visual 'scene environment'.
@@ -17,16 +19,16 @@ int main()
     x.linspace (-0.5, 0.8, 14);
 
     // Set a graph up of x^3
-    std::string ds1legend = morph::unicode::toUtf8 (morph::unicode::alpha);
-    ds1legend += "(x) = x" + morph::unicode::toUtf8 (morph::unicode::ss3);
+    std::string ds1legend = unicode::toUtf8 (unicode::alpha);
+    ds1legend += "(x) = x" + unicode::toUtf8 (unicode::ss3);
     gv->setdata (x, x.pow(3), ds1legend);
-    gv->ylabel = morph::unicode::toUtf8 (morph::unicode::alpha);
+    gv->ylabel = unicode::toUtf8 (unicode::alpha);
 
     // And 100x^2
-    std::string ds2legend = morph::unicode::toUtf8 (morph::unicode::beta);
-    ds2legend += "(x) = 100x" + morph::unicode::toUtf8 (morph::unicode::ss2);
+    std::string ds2legend = unicode::toUtf8 (unicode::beta);
+    ds2legend += "(x) = 100x" + unicode::toUtf8 (unicode::ss2);
     gv->setdata_rightaxis (x, x.pow(2)*100, ds2legend);
-    gv->ylabel2 = morph::unicode::toUtf8 (morph::unicode::beta);
+    gv->ylabel2 = unicode::toUtf8 (unicode::beta);
 
     // finalize() makes the GraphVisual compute the vertices of the OpenGL model
     gv->finalize();
