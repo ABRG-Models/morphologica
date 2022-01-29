97c4767bee0eab6970c17747314cbdba1bf4bf49
Seb James <seb.james@sheffield.ac.uk>
2022-01-29 08:49:36 +0000

    Tweaks legend strings

--- examples/graph5.cpp
+++ examples/graph5.cpp
@@ -9,15 +9,25 @@ int main()
     morph::Visual v(1024, 768, "Twinax GraphVisual example");
     // Create a new GraphVisual with offset within the scene of 0,0,0
     auto gv = new morph::GraphVisual<double> (v.shaderprog, v.tshaderprog, {0,0,0});
+    // This is going to be a twin axis graph
+    gv->axisstyle = morph::axisstyle::twinax;
     // Data for the x axis. A vVector is like std::vector, but with built-in maths methods
     morph::vVector<double> x;
     // This works like numpy's linspace() (the 3 args are "start", "end" and "num"):
     x.linspace (-0.5, 0.8, 14);
-    // Set a graph up of y = x^3
-    gv->setdata (x, x.pow(3));
-    // And x sq
-    gv->axisstyle = morph::axisstyle::twinax;
-    gv->setdata_rightaxis (x, x.pow(2)*100);
+
+    // Set a graph up of x^3
+    std::string ds1legend = morph::unicode::toUtf8 (morph::unicode::alpha);
+    ds1legend += "(x) = x" + morph::unicode::toUtf8 (morph::unicode::ss3);
+    gv->setdata (x, x.pow(3), ds1legend);
+    gv->ylabel = morph::unicode::toUtf8 (morph::unicode::alpha);
+
+    // And 100x^2
+    std::string ds2legend = morph::unicode::toUtf8 (morph::unicode::beta);
+    ds2legend += "(x) = 100x" + morph::unicode::toUtf8 (morph::unicode::ss2);
+    gv->setdata_rightaxis (x, x.pow(2)*100, ds2legend);
+    gv->ylabel2 = morph::unicode::toUtf8 (morph::unicode::beta);
+
     // finalize() makes the GraphVisual compute the vertices of the OpenGL model
     gv->finalize();
     // Add the GraphVisual OpenGL model to the Visual scene
