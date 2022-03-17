ABRG-Models/morphologica

commit fe629c24d81aa180aa73f7f07a54462699fa3eb5

Resolves a long standing memory leak with VisualModels and VisualTextModels

morph/VisualModel.h
@@ -103,6 +103,7 @@ namespace morph {
                 glDeleteBuffers (numVBO, this->vbos);
                 morph::gl::Util::checkError (__FILE__, __LINE__);
                 delete[] this->vbos;
+                glDeleteVertexArrays (1, &this->vao);
             }
         }
 
@@ -200,6 +201,7 @@ namespace morph {
             this->vertexNormals.clear();
             this->vertexColors.clear();
             this->indices.clear();
+            this->clearTexts();
             this->idx = 0;
             this->initializeVertices();
             this->reinit_buffers();

morph/VisualTextModel.h
@@ -122,6 +122,7 @@ namespace morph {
             if (this->vbos != nullptr) {
                 glDeleteBuffers (numVBO, this->vbos);
                 delete[] this->vbos;
+                glDeleteVertexArrays (1, &this->vao);
             }
         }
 

commit cc0a6be51c6d0b995481afbe5e2cbe90b7bec15b

Fix bug introduced with fe629c24. Don't clear those texts in reinit()!

morph/VisualModel.h
@@ -181,7 +181,7 @@ namespace morph {
             this->texts.clear();
         }
 
-        //! Clear out the model
+        //! Clear out the model, *including text models*
         void clear()
         {
             this->vertexPositions.clear();
@@ -201,7 +201,7 @@ namespace morph {
             this->vertexNormals.clear();
             this->vertexColors.clear();
             this->indices.clear();
-            this->clearTexts();
+            // NB: Do NOT call clearTexts() here! We're only updating the model itself.
             this->idx = 0;
             this->initializeVertices();
             this->reinit_buffers();