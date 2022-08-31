c3f1cd6ea8ca818b48df742f6de50867700daad3
Seb James <seb.james@sheffield.ac.uk>
2022-08-31 17:08:42 +0100

    Adds an XOR example (with visualization)

---
+++ standalone_examples/neuralnet/ff_xor.cpp
@@ -0,0 +1,140 @@
+/*
+ * Train a small feedforward neural network on the XOR problem.
+ *
+ * \author Seb James
+ * \date Aug 2022
+ */
+
+#include <morph/Mnist.h>
+#include <morph/Random.h>
+#include <morph/nn/FeedForwardNet.h>
+#include <morph/vVector.h>
+#include <fstream>
+#include <vector>
+#include <utility>
+#include <sstream>
+
+#include <morph/Visual.h>
+#include <morph/GraphVisual.h>
+#include "netvisual.h"
+
+int main (int argc, char** argv)
+{
+    // Create a feed-forward network
+    std::vector<unsigned int> layer_spec = {2,5,1};
+    morph::nn::FeedForwardNet<float> ff1(layer_spec);
+    std::cout << ff1 << std::endl;
+
+    // The XOR function has 4 possible inputs and 4 correct outputs.
+    std::vector<morph::vVector<float>> ins = {{1, 0}, {0, 1}, {0, 0}, {1, 1}};
+    std::vector<morph::vVector<float>> outs = {{1}, {1}, {0}, {0}};
+
+    // main loop, while m.training_f has values in:
+    unsigned int epochs = 3000; // Here, an epoch is a run through each batch of in/outs.
+    unsigned int mini_batch_size = ins.size();
+    float eta = 0.5f;
+    std::stringstream ss;
+    if (argc > 1) {
+        ss << argv[1];
+        ss >> eta;
+    }
+    std::cout << "eta = " << eta << std::endl;
+    float cost = 0.0f;
+
+    // Accumulate the dC/dw and dC/db values in gradients. for each pair, the first
+    // is nabla_w the second is nabla_b. There are as many pairs as there are
+    // connections in ff1.
+    std::vector<std::pair<morph::vVector<float>, morph::vVector<float>>> mean_gradients;
+    // Init mean gradients
+    for (auto& c : ff1.connections) {
+        mean_gradients.push_back (std::make_pair(c.nabla_ws[0], c.nabla_b));
+    }
+    for (auto g : mean_gradients) {
+        std::cout << "nabla_w: " << g.first << ", nabla_b: " << g.second << std::endl;
+    }
+
+    std::ofstream costfile;
+    costfile.open ("cost.csv", std::ios::out|std::ios::trunc);
+
+    morph::vVector<float> costs(epochs, 0.0f);
+    morph::vVector<float> epoch_container(epochs, 0);
+
+    // Used throughout as an iterator variable
+    unsigned int i = 0;
+    for (unsigned int ep = 0; ep < epochs; ++ep) {
+
+        // Zero mean grads
+        for (i = 0; i < ff1.connections.size(); ++i) {
+            mean_gradients[i].first.zero();
+            mean_gradients[i].second.zero();
+        }
+
+        cost = 0.0f;
+        for (unsigned int mb = 0; mb < mini_batch_size; ++mb) {
+
+            // Note: NOT stochastic!
+            morph::vVector<float> thein = ins[mb];
+            morph::vVector<float> theout = outs[mb];
+
+            ff1.setInput (thein, theout);
+            ff1.feedforward();
+            cost += ff1.computeCost();
+            ff1.backprop();
+            //std::cout << "After ff1.backprop():\n---------------\n" << ff1 << std::endl;
+
+            // Now collect up the cost, the nabla_w and nabla_bs for the learning step
+            i = 0;
+            for (auto& c : ff1.connections) {
+                mean_gradients[i].first += c.nabla_ws[0];
+                mean_gradients[i].second += c.nabla_b;
+                ++i;
+            }
+        }
+
+        // Have accumulated cost and mean_gradients, so now divide through to get the means
+        for (i = 0; i < ff1.connections.size(); ++i) {
+            mean_gradients[i].first /= static_cast<float>(mini_batch_size);
+            mean_gradients[i].second /= static_cast<float>(mini_batch_size);
+        }
+        cost /= (static_cast<float>(mini_batch_size));
+        costfile << cost << std::endl;
+
+        // Store for graph
+        costs[ep] = cost;
+        epoch_container[ep] = static_cast<float>(ep);
+
+        // Gradient update. v -> v' = v - eta * gradC
+        i = 0;
+        for (auto& c : ff1.connections) {
+            c.ws[0] -= (mean_gradients[i].first * eta);
+            c.b -= (mean_gradients[i].second * eta);
+            ++i;
+        }
+    }
+    costfile.close();
+
+    std::cout << "Evaluate final network:\n================\n";
+    ff1.evaluate (ins, outs);
+    std::cout << "FINAL NETWORK:\n================\n" << ff1;
+
+    // Visualise the network
+    morph::Visual v(1000, 1000, "XOR network");
+    v.lightingEffects();
+    NetVisual<float>* nv = new NetVisual<float>(v.shaderprog, v.tshaderprog, {0,0,0}, &ff1);
+    nv->finalize();
+    v.addVisualModel (nv);
+
+    morph::GraphVisual<float>* gv = new morph::GraphVisual<float>(v.shaderprog, v.tshaderprog, {1,0.5,0});
+    morph::DatasetStyle ds(morph::stylepolicy::lines);
+    ds.linecolour =  {1.0, 0.0, 0.0};
+    ds.linewidth = 0.015f;
+    gv->xlabel = "Epoch";
+    gv->ylabel = "Cost";
+    gv->setdata (epoch_container, costs, ds);
+    gv->finalize();
+    v.addVisualModel (gv);
+
+    v.keepOpen();
+
+    return 0;
+}
---
+++ standalone_examples/neuralnet/netvisual.h
@@ -0,0 +1,140 @@
+/*!
+ * \file
+ *
+ * Visualise a neural network
+ *
+ * \author Seb James
+ * \date 2022
+ */
+#pragma once
+
+#ifdef __OSX__
+# include <OpenGL/gl3.h>
+#else
+# include <GL3/gl3.h>
+#endif
+#include <morph/VisualModel.h>
+#include <morph/Vector.h>
+#include <morph/Scale.h>
+#include <vector>
+#include <array>
+
+#include <morph/nn/FeedForwardNet.h>
+
+template <typename Flt>
+class NetVisual : public morph::VisualModel
+{
+public:
+    NetVisual(GLuint sp, GLuint tsp, const morph::Vector<float, 3> _offset, morph::nn::FeedForwardNet<Flt>* _thenet)
+    {
+        this->nn = _thenet;
+        this->shaderprog = sp;
+        this->tshaderprog = tsp;
+        this->mv_offset = _offset;
+        this->viewmatrix.translate (this->mv_offset);
+    }
+
+    void initializeVertices()
+    {
+        VBOint idx = 0;
+
+        // For each neuron layer, draw discs, with colour indicating activation.
+        // For each connection layer, draw lines, with colours indicating weight?
+
+        // Draw neurons in a pattern; this position is updated as each is drawn.
+        morph::Vector<float,3> nloc = {0,0,0};
+        std::array<float,3> clr = {1,0,0};
+
+        // Starting locations for each set of neurons, to help draw connection lines
+        std::vector<morph::Vector<float,3>> startlocs;
+
+        // Scaling for colour
+        morph::Scale<float> s;
+        float min_act = this->nn->min_neuron_activation();
+        float max_act = this->nn->max_neuron_activation();
+        s.compute_autoscale (min_act, max_act);
+
+        // Our colour map
+        morph::ColourMap<float> cmf(morph::ColourMapType::Plasma);
+
+        for (auto nlayer : this->nn->neurons) {
+            // Each nlayer is vVector<T>
+            nloc[1] += this->radiusFixed * 5.0f;
+            nloc[0] = 0.0f;
+            nloc[0] -= this->radiusFixed * 2.0f * nlayer.size();
+            startlocs.push_back (nloc);
+            for (auto neuron : nlayer) {
+                // Use colour from neuron value.
+                clr = cmf.convert (s.transform_one(neuron));
+
+                this->computeTube (idx,
+                                   (nloc+this->puckthick)*zoom,
+                                   (nloc-this->puckthick)*zoom,
+                                   morph::Vector<float,3>({1,0,0}), morph::Vector<float,3>({0,1,0}),
+                                   clr, clr,
+                                   this->radiusFixed*zoom, 16);
+                nloc[0] += this->radiusFixed * 4.0f;
+            }
+
+        }
+
+        // Connections. There should be neurons.size()-1 connection layers:
+        // std::list<morph::nn::FeedForwardConn<T>> connections;
+        // Connection lines
+        float min_weight = -1;
+        float max_weight = 1;
+        s.compute_autoscale (min_weight, max_weight);
+        morph::Vector<float,3> nloc2 = {0,0,0};
+        nloc = {0,0,0};
+        auto sl = startlocs.begin();
+        for (auto cl : this->nn->connections) {
+
+            std::vector<morph::vVector<float>>& ws = cl.ws;
+            nloc = *sl;
+            nloc2 = *(++sl); --sl;
+            std::cout << "0) nloc = " << nloc << std::endl;
+            std::cout << "0) nloc2 = " << nloc2 << std::endl;
+            for (auto population : ws) { // FeedForwardConn can accept connections from multiple neuron layers.
+                // Set output position
+                unsigned int counter = 0;
+                for (auto w : population) {
+
+                    std::cout << "nloc = " << nloc << std::endl;
+                    std::cout << "nloc2 = " << nloc2 << std::endl;
+                    // Draw connection line from w's input position
+                    clr = cmf.convert (s.transform_one(w));
+
+                    this->computeLine (idx, nloc, nloc2, this->uz, clr, clr,
+                                       this->linewidth*zoom, this->linewidth/4*zoom);
+
+                    // When reset is needed:
+                    size_t M = population.size() / cl.N;
+                    std::cout << "counter+1=" << (counter+1) << " > " << "M=" << M << "?\n";
+                    if (++counter >= M) {
+                        std::cout << "reset nloc/nloc2\n";
+                        nloc = *sl; // reset nloc
+                        nloc2[0] += this->radiusFixed * 4.0f; // inc nloc2
+                        counter = 0;
+                    } else {
+                        std::cout << "No reset\n";
+                        nloc[0] += this->radiusFixed * 4.0f;
+                        // nloc2 unchanged
+                    }
+
+                }
+            }
+            sl++;
+        }
+    }
+
+    //! Pointer to a vector of locations to visualise
+    morph::nn::FeedForwardNet<Flt>* nn = nullptr;
+    Flt radiusFixed = 0.1;
+    Flt linewidth = 0.02;
+    //! Zoom the size of the netvisual
+    float zoom = float{1};
+
+    morph::Vector<float,3> puckthick = { 0, 0, 0.02 };
+    //! A normal vector, fixed as pointing up
+    morph::Vector<float, 3> uz = {0,0,1};
+};
