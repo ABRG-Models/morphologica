cda00f0fc319c68add7dae8a2ae72dc59e990e59
Seb James <seb@opteran.com>
2023-09-01 12:06:01 +0100

    Adds an HSVWheel widget thing to go with your HSV colourmaps

---
+++ morph/HSVWheelVisual.h
@@ -0,0 +1,184 @@
+/*
+ * A visual to label HSV graphs
+ */
+
+#pragma once
+
+#ifndef USE_GLEW
+#ifdef __OSX__
+# include <OpenGL/gl3.h>
+#else
+# include <GL3/gl3.h>
+#endif
+#endif
+#include <morph/mathconst.h>
+#include <morph/Scale.h>
+#include <morph/vec.h>
+#include <morph/GraphVisual.h>
+
+namespace morph {
+
+    template <typename F>
+    class HSVWheelVisual : public VisualModel
+    {
+        using mc = morph::mathconst<F>;
+    public:
+        //! Constructor
+        //! \param _offset The offset within morph::Visual space to place this model
+        HSVWheelVisual (const vec<float> _offset)
+        {
+            this->mv_offset = _offset;
+            this->viewmatrix.translate (this->mv_offset);
+            // Set default text features
+            this->tf.fontsize = 0.05f;
+            this->tf.fontres = 48;
+            this->tf.colour = this->framecolour;
+            // Like graphs, hsv wheels don't rotate by default. If you want yours to, set this false in your client code.
+            this->twodimensional = true;
+        }
+
+        void setTextColour (const std::array<float, 3>& c)
+        {
+            this->tf.colour = c;
+        }
+
+        void setFrameColour (const std::array<float, 3>& c)
+        {
+            this->framecolour = c;
+        }
+
+        void setColour (const std::array<float, 3>& c)
+        {
+            this->tf.colour = c;
+            this->framecolour = c;
+        }
+
+        void initializeVertices()
+        {
+            // Auto-set ticklabelgap
+            auto em = std::make_unique<morph::VisualTextModel> (this->parentVis, this->get_tprog(this->parentVis), this->tf);
+            morph::TextGeometry em_geom = em->getTextGeometry (std::string("m"));
+            this->ticklabelgap = em_geom.width()/2.0f;
+            this->drawFrame();
+            this->drawTickLabels();
+            this->fillFrameWithColour();
+        }
+
+        //! Draw a circular frame around the wheel
+        void drawFrame()
+        {
+            // Draw an approximation of a circle.
+            this->computeFlatCircleLine (this->idx, vec<float>({0,0,this->z}), this->uz, this->radius + this->framelinewidth/2.0f,
+                                         this->framelinewidth, this->framecolour, this->numsegs);
+        }
+
+        //! Draw the tick labels (the numbers or whatever text the client code has given us)
+        void drawTickLabels()
+        {
+            // Reset these members
+            this->ticklabelheight = 0.0f;
+            this->ticklabelwidth = 0.0f;
+
+            if (this->tick_posns.empty()) {
+                // Auto-fill tick_posns based on ticks size.
+                // example order for 4: mc::pi_over_2, mc::pi, mc::three_pi_over_2, 0.0f
+                this->tick_posns.resize (this->ticks.size());
+                for (unsigned int i = 0; i < this->ticks.size(); ++i) {
+                    // North is pi/2, so that's the start:
+                    this->tick_posns[i] = mc::pi_over_2 + i * (mc::two_pi / this->ticks.size());
+                    // Rescale any that exceed 2pi:
+                    this->tick_posns[i] = this->tick_posns[i] < F{0} ? this->tick_posns[i] + mc::two_pi : this->tick_posns[i];
+                    this->tick_posns[i] = this->tick_posns[i] > mc::two_pi ? this->tick_posns[i] - mc::two_pi : this->tick_posns[i];
+                }
+            }
+
+            for (unsigned int i = 0; i < this->tick_posns.size(); ++i) {
+                std::string s = this->ticks[i];
+                auto lbl = std::make_unique<morph::VisualTextModel> (this->parentVis, this->get_tprog(this->parentVis), this->tf);
+                morph::TextGeometry geom = lbl->getTextGeometry (s);
+                this->ticklabelheight = geom.height() > this->ticklabelheight ? geom.height() : this->ticklabelheight;
+                this->ticklabelwidth = geom.width() > this->ticklabelwidth ? geom.width() : this->ticklabelwidth;
+                morph::vec<float> lblpos = {
+                    (this->radius + this->framelinewidth + this->ticklabelgap + 0.5f*(geom.half_width() + geom.half_height())) * std::cos (tick_posns[i]) - geom.half_width(),
+                    (this->radius + this->framelinewidth + this->ticklabelgap + 0.5f*(geom.half_width() + geom.half_height())) * std::sin (tick_posns[i]) - geom.half_height(),
+                    this->z
+                };
+                lbl->setupText (s, lblpos+this->mv_offset, this->tf.colour);
+                this->texts.push_back (std::move(lbl));
+            }
+        }
+
+        // Draw the actual HSV stuff
+        void fillFrameWithColour()
+        {
+            vec<float> centre = {0,0,this->z};
+
+            for (int ring = this->numrings; ring > 0; ring--) {
+
+                float r_out = this->radius * static_cast<float>(ring)/this->numrings;
+                float r_in = this->radius * static_cast<float>(ring-1)/this->numrings;
+                float norm_r_out = r_out / this->radius; // range 0->1
+                float norm_r_in = r_in / this->radius;
+
+                for (int j = 0; j < static_cast<int>(this->numsegs); j++) {
+
+                    // The colour will change for each j
+                    float colour_angle = (static_cast<float>(j)/this->numsegs) * morph::mathconst<float>::two_pi;
+                    std::array<float, 3> col_out = this->cm.convert_angular (colour_angle, norm_r_out);
+                    std::array<float, 3> col_in = this->cm.convert_angular (colour_angle, norm_r_in);
+
+                    float t = j * morph::mathconst<float>::two_pi/static_cast<float>(this->numsegs);
+                    vec<float> c_in = this->uy * sin(t) * r_in + this->ux * cos(t) * r_in;
+                    this->vertex_push (centre+c_in, this->vertexPositions);
+                    this->vertex_push (this->uz, this->vertexNormals);
+                    this->vertex_push (col_in, this->vertexColors);
+                    vec<float> c_out = this->uy * sin(t) * r_out + this->ux * cos(t) * r_out;
+                    this->vertex_push (centre+c_out, this->vertexPositions);
+                    this->vertex_push (this->uz, this->vertexNormals);
+                    this->vertex_push (col_out, this->vertexColors);
+                }
+                // Added 2*segments vertices to vertexPositions
+
+                for (int j = 0; j < static_cast<int>(this->numsegs); j++) {
+                    int jn = (numsegs + ((j+1) % numsegs)) % numsegs;
+                    this->indices.push_back (this->idx+(2*j));
+                    this->indices.push_back (this->idx+(2*jn));
+                    this->indices.push_back (this->idx+(2*jn+1));
+                    this->indices.push_back (this->idx+(2*j));
+                    this->indices.push_back (this->idx+(2*jn+1));
+                    this->indices.push_back (this->idx+(2*j+1));
+                }
+                this->idx += 2 * this->numsegs; // nverts
+            }
+        }
+
+        //! The ColourMap to show (copy in). Should be type ColourMapType::HSV
+        morph::ColourMap<F> cm;
+        //! The radius of the HSVwheel
+        float radius = 1.0f;
+        //! Position in z in model space. Default is just 0.
+        float z = 0.0f;
+        //! colour for the axis box/lines. Text colour is in TextFeatures tf.colour
+        std::array<float, 3> framecolour = morph::colour::black;
+        //! The line width of the frame
+        float framelinewidth = 0.01f;
+        //! The tick strings  that should be displayed. Order the elements anti-clockwise, starting from the 'north' element.
+        std::deque<std::string> ticks = {"N", "W", "S", "E"};
+        //! The positions, as angles for the ticks/tick labels. If empty, these will be auto-computed
+        std::deque<F> tick_posns = { /*mc::pi_over_2, mc::pi, mc::three_pi_over_2, 0.0f*/ };
+        // Stores all the text features for this ColourBar (font, colour, font size, font res)
+        morph::TextFeatures tf;
+        //! Gap to x axis tick labels. Gets auto-set
+        float ticklabelgap = 0.05f;
+        //! The number of segments to make in each ring of the colourmap fill
+        unsigned int numsegs = 128;
+        //! How many rings of colour? Colours blend perfectly, so only need 1! Any other number will look the same.
+        unsigned int numrings = 1;
+    protected:
+        //! tick label height
+        float ticklabelheight = 0.0f;
+        //! tick label width
+        float ticklabelwidth = 0.0f;
+    };
+
+} // namespace
