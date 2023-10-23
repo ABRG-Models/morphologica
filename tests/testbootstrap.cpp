38c001fca53ee3edfb99f23b699c32ed2cd6f009
Seb James <sjameswml@gmail.com>
2023-10-23 20:49:03 +0100

    Hopefully fixes the testbootstrap failing sometimes

--- tests/testbootstrap.cpp
+++ tests/testbootstrap.cpp
@@ -34,33 +34,45 @@ int main()
         --rtn;
     }
 
-    // Now test the t-test:
+    // Now test the t-test 100 times:
+    int n = 0;
+    int sig_diff_fails = 0;
+    int not_sig_diff_fails = 0;
+    morph::vvec<double> non_sig_diffs (100, 0.0);
+    while (n < 100) {
+        // First a significantly different distribution
+        morph::RandNormal<double, std::mt19937_64> rnorm_sigdiff (5.5, 1);
+        morph::vvec<double> dist_sigdiff;
+        dist_sigdiff.set_from (rnorm_sigdiff.get(num_samples));
+        morph::vec<double,2> asl_sigdiff = morph::bootstrap<double>::ttest_equalityofmeans (normally_distributed, dist_sigdiff, num_bootstraps);
+        std::cout << "Achieved significance level for different dist: " << asl_sigdiff[0] << " (with minasl: " << asl_sigdiff[1] << ")\n";
+        // asl_sigdiff[0] should be 0.
+        if (asl_sigdiff[0] > asl_sigdiff[1]) {
+            std::cerr << "The distribution with a different mean has ASL = " << asl_sigdiff[0] << " which is TOO BIG.\n";
+            sig_diff_fails++;
+        }
 
-    // First a significantly different distribution
-    morph::RandNormal<double, std::mt19937_64> rnorm_sigdiff (5.2, 1);
-    morph::vvec<double> dist_sigdiff;
-    dist_sigdiff.set_from (rnorm_sigdiff.get(num_samples));
-    morph::vec<double,2> asl_sigdiff = morph::bootstrap<double>::ttest_equalityofmeans (normally_distributed, dist_sigdiff, num_bootstraps);
-    std::cout << "Achieved significance level for different dist: " << asl_sigdiff[0] << " (with minasl: " << asl_sigdiff[1] << ")\n";
-    // asl_sigdiff[0] should be 0.
-    if (asl_sigdiff[0] > asl_sigdiff[1]) {
-        std::cerr << "The distribution with a different mean has ASL = " << asl_sigdiff[0] << " which is too big.\n";
-        --rtn;
+        // Now a not significantly different (in terms of the mean) distribution
+        morph::RandNormal<double, std::mt19937_64> rnorm_nonsigdiff (5.0, 1.5);
+        morph::vvec<double> dist_nonsigdiff;
+        dist_nonsigdiff.set_from (rnorm_nonsigdiff.get(num_samples));
+        morph::vec<double,2> asl_nonsigdiff = morph::bootstrap<double>::ttest_equalityofmeans (normally_distributed, dist_nonsigdiff, num_bootstraps);
+        std::cout << "Achieved significance level for non-different dist: " << asl_nonsigdiff[0] << " (with minasl: " << asl_nonsigdiff[1] << ")\n";
+        // asl_nonsigdiff[0] should be about 0.4, but can be as low as 0.044
+        non_sig_diffs[n] = asl_nonsigdiff[0];
+        if (asl_nonsigdiff[0] < 0.001) {
+            std::cerr << "The distribution with a NON different mean has ASL = " << asl_nonsigdiff[0] << " which is TOO SMALL.\n";
+            not_sig_diff_fails++;
+        }
+        ++n;
     }
 
-    // Now a not significantly different (in terms of the mean) distribution
-    morph::RandNormal<double, std::mt19937_64> rnorm_nonsigdiff (5.0, 1.5);
-    morph::vvec<double> dist_nonsigdiff;
-    dist_nonsigdiff.set_from (rnorm_nonsigdiff.get(num_samples));
-    morph::vec<double,2> asl_nonsigdiff = morph::bootstrap<double>::ttest_equalityofmeans (normally_distributed, dist_nonsigdiff, num_bootstraps);
-    std::cout << "Achieved significance level for non-different dist: " << asl_nonsigdiff[0] << " (with minasl: " << asl_nonsigdiff[1] << ")\n";
-    // asl_nonsigdiff[0] should be about 0.4, but can be as low as 0.044
-    if (asl_nonsigdiff[0] < 0.001) {
-        std::cerr << "The distribution with a NON different mean has ASL = " << asl_nonsigdiff[0] << " which is too small.\n";
-        --rtn;
-    }
+    morph::range<double> r = non_sig_diffs.minmax();
+    std::cout << "Range of significance levels for non different distributions was " << r << std::endl;
 
-    if (rtn == 0) {
+    // Statistical variability seems to allow quite a few unexpected results. Pass the
+    // test as long as we have less than 10% fails in both types of test
+    if (sig_diff_fails < 10 && not_sig_diff_fails < 10) {
         std::cout << "Test passed.\n";
     } else {
         std::cout << "Test FAILED.\n";
