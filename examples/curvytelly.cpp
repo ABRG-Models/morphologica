36f65b2b3a6b49056629401fd08274386104960d
Seb James <sjameswml@gmail.com>
2024-07-31 15:11:33 +0100

    An improved CurvyTellyVisual example

--- examples/curvytelly.cpp
+++ examples/curvytelly.cpp
@@ -5,6 +5,7 @@
 #include <morph/vec.h>
 #include <morph/vvec.h>
 #include <morph/Grid.h>
+#include <morph/Quaternion.h>
 #include <morph/loadpng.h>
 #include <morph/Visual.h>
 #include <morph/CurvyTellyVisual.h>
@@ -13,31 +14,30 @@ int main()
 {
     morph::Visual<> v(1600, 1000, "CurvyTellyVisual showing an image");
 
-    std::string fn = "../examples/horsehead_reduced.png";
-    //morph::vec<unsigned int, 2> dims = morph::loadpng (fn, image_data_tlbr, morph::vec<bool, 2>{false,false});
+    // Load an image (you have to run the program from ./build/
     morph::vvec<float> image_data;
-    morph::vec<unsigned int, 2> dims = morph::loadpng (fn, image_data);
-    std::cout << "Image dims: " << dims << std::endl;
+    morph::vec<unsigned int, 2> dims = morph::loadpng ("../examples/horsehead_reduced.png", image_data);
 
-    // Make a Grid to display the stripes.
-    // In x, make it as many wide as there will be facets on the tube.
-    // In y, make it as many long as you want there to be stripes
-    // Choose x/y spacing to suit required circumference and length
+    // CurvyTellyVisual needs a Grid as an underlying data structure
     morph::vec<float, 2> grid_spacing = { 0.1f, 0.01f };
     morph::Grid grid(dims[0], dims[1], grid_spacing);
-    std::cout << "Number of pixels in grid:" << grid.n << std::endl;
 
     morph::vec<float> offset = { 0, 0, 0 };
     auto ctv = std::make_unique<morph::CurvyTellyVisual<float>>(&grid, offset);
     v.bindmodel (ctv);
     ctv->setScalarData (&image_data);
-    ctv->radius = 10.0f;
+    ctv->cm.setType (morph::ColourMapType::Magma);
+    ctv->radius = 10.0f;     // The radius of curvature of the telly
+    ctv->centroidize = true; // Ensures the centre of the VisualModel is the 'middle of the screen' (it's centroid)
     ctv->angle_to_subtend = morph::mathconst<float>::pi_over_3; // 2pi is default
-    //ctv->tb_frames = false;
-    //ctv->lr_frames = false;
-    //ctv->cm.setType (morph::ColourMapType::Greyscale);
+    ctv->frame_width = 0.1f; // Show a frame around the image
+    ctv->frame_clr = morph::colour::navy;
     ctv->finalize();
     v.addVisualModel (ctv);
+
+    // To make this view in the correct orientation as if it were a TV, we have to rotate & translate the scene.
+    v.setSceneTrans (morph::vec<float,3>{ float{0}, float{0}, float{-14} });
+    v.setSceneRotation (morph::Quaternion<float>{ float{0.5}, float{0.5}, float{-0.5}, float{-0.5} });
 
     v.keepOpen();
 
