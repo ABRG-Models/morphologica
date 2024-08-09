2d56fc4471aeecd4bb343ecd1c230816f1284092
Seb James <sjameswml@gmail.com>
2024-08-09 10:55:16 +0100

    Encodes some more ruby knowledge to assist in running jekyll

--- docs/run_local.sh
+++ docs/run_local.sh
@@ -5,5 +5,7 @@
 echo "Assuming you already installed a ruby environment, with a command like:"
 echo "  sudo apt install ruby ruby-dev"
 echo "AND assuming you already installed jekyll, etc with:"
+echo "  export GEM_HOME=~/gems        # put this in your .bashrc"
+echo "  export PATH=\${GEM_HOME}/bin  # along with this then you won't need --install-dir ~/gems below:"
 echo "  gem install --install-dir ~/gems bundler jekyll jekyll-default-layout just-the-docs github-pages"
 bundle exec jekyll serve --livereload &
