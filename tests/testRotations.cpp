2a4f7b069d9e95cd67c7dd6b6ace0f706016ce80
Seb James <sjameswml@gmail.com>
2024-09-28 23:26:57 +0100

    More tests

--- tests/testRotations.cpp
+++ tests/testRotations.cpp
@@ -13,36 +13,63 @@ int main()
 
     using mc = morph::mathconst<float>;
 
+    morph::Quaternion<float> qx (ux, mc::pi_over_2);
+    morph::vec<float> ux_about_x = qx * ux;
+    morph::vec<float> uy_about_x = qx * uy;
+    morph::vec<float> uz_about_x = qx * uz;
+    std::cout << "ux: " << ux << " rotated about the x axis is " << ux_about_x << std::endl;
+    std::cout << "uy: " << uy << " rotated about the x axis is " << uy_about_x << std::endl;
+    std::cout << "uz: " << uz << " rotated about the x axis is " << uz_about_x << std::endl;
+
+    morph::Quaternion<float> qy (uy, mc::pi_over_2);
+    morph::vec<float> ux_about_y = qy * ux;
+    morph::vec<float> uy_about_y = qy * uy;
+    morph::vec<float> uz_about_y = qy * uz;
+    std::cout << std::endl
+              << "ux: " << ux << " rotated about the y axis is " << ux_about_y << std::endl;
+    std::cout << "uy: " << uy << " rotated about the y axis is " << uy_about_y << std::endl;
+    std::cout << "uz: " << uz << " rotated about the y axis is " << uz_about_y << std::endl;
+
     morph::Quaternion<float> qz (uz, mc::pi_over_2);
-
-    morph::vec<float> uy_about_z = qz * uy;
     morph::vec<float> ux_about_z = qz * ux;
+    morph::vec<float> uy_about_z = qz * uy;
     morph::vec<float> uz_about_z = qz * uz;
-
-    std::cout << "ux: " << ux << " rotated about the z axis is " << ux_about_z << std::endl;
+    std::cout << std::endl
+              << "ux: " << ux << " rotated about the z axis is " << ux_about_z << std::endl;
     std::cout << "uy: " << uy << " rotated about the z axis is " << uy_about_z << std::endl;
     std::cout << "uz: " << uz << " rotated about the z axis is " << uz_about_z << std::endl;
 
-    morph::TransformMatrix<float> tm;
-    tm.rotate (qz);
+    std::cout << "\n\n";
 
-    morph::vec<float, 4> ux_about_z_tm = tm * ux;
-    morph::vec<float, 4> uy_about_z_tm = tm * uy;
-    morph::vec<float, 4> uz_about_z_tm = tm * uz;
+    morph::TransformMatrix<float> tmx;
+    tmx.rotate (qx);
+    morph::vec<float, 4> ux_about_tmx = tmx * ux;
+    morph::vec<float, 4> uy_about_tmx = tmx * uy;
+    morph::vec<float, 4> uz_about_tmx = tmx * uz;
+    std::cout << "ux: " << ux << " rotated about the x axis by TM is " << ux_about_tmx << std::endl;
+    std::cout << "uy: " << uy << " rotated about the x axis by TM is " << uy_about_tmx << std::endl;
+    std::cout << "uz: " << uz << " rotated about the x axis by TM is " << uz_about_tmx << std::endl;
 
-    std::cout << "ux: " << ux << " rotated about the z axis by TM is " << ux_about_z_tm << std::endl;
-    std::cout << "uy: " << uy << " rotated about the z axis by TM is " << uy_about_z_tm << std::endl;
-    std::cout << "uz: " << uz << " rotated about the z axis by TM is " << uz_about_z_tm << std::endl;
+    morph::TransformMatrix<float> tmy;
+    tmy.rotate (qy);
+    morph::vec<float, 4> ux_about_tmy = tmy * ux;
+    morph::vec<float, 4> uy_about_tmy = tmy * uy;
+    morph::vec<float, 4> uz_about_tmy = tmy * uz;
+    std::cout << std::endl
+              << "ux: " << ux << " rotated about the y axis by TM is " << ux_about_tmy << std::endl;
+    std::cout << "uy: " << uy << " rotated about the y axis by TM is " << uy_about_tmy << std::endl;
+    std::cout << "uz: " << uz << " rotated about the y axis by TM is " << uz_about_tmy << std::endl;
 
-    morph::TransformMatrix<float> tm2;
-    tm2.rotate (uz, mc::pi_over_2);
+    morph::TransformMatrix<float> tmz;
+    tmz.rotate (qz);
+    morph::vec<float, 4> ux_about_tmz = tmz * ux;
+    morph::vec<float, 4> uy_about_tmz = tmz * uy;
+    morph::vec<float, 4> uz_about_tmz = tmz * uz;
+    std::cout << std::endl
+              << "ux: " << ux << " rotated about the z axis by TM is " << ux_about_tmz << std::endl;
+    std::cout << "uy: " << uy << " rotated about the z axis by TM is " << uy_about_tmz << std::endl;
+    std::cout << "uz: " << uz << " rotated about the z axis by TM is " << uz_about_tmz << std::endl;
 
-    morph::vec<float, 4> ux_about_z_tm2 = tm2 * ux;
-    morph::vec<float, 4> uy_about_z_tm2 = tm2 * uy;
-    morph::vec<float, 4> uz_about_z_tm2 = tm2 * uz;
 
-    std::cout << "ux: " << ux << " rotated about the z axis by TM2 is " << ux_about_z_tm2 << std::endl;
-    std::cout << "uy: " << uy << " rotated about the z axis by TM2 is " << uy_about_z_tm2 << std::endl;
-    std::cout << "uz: " << uz << " rotated about the z axis by TM2 is " << uz_about_z_tm2 << std::endl;
     return 0;
 }
