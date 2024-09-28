3e1af88833081679d2786376b2f710dcb48f741d
Seb James <sjameswml@gmail.com>
2024-09-29 00:17:44 +0100

    adds comments about the other way to set rotation

--- morph/TransformMatrix.h
+++ morph/TransformMatrix.h
@@ -466,7 +466,8 @@ namespace morph {
         /*!
          * This algorithm was obtained from:
          * http://www.j3d.org/matrix_faq/matrfaq_latest.html#Q54 (but was it transposed?
-         * seems so. See also https://www.songho.ca/opengl/gl_quaternion.html#overview)
+         * seems so. See also https://www.songho.ca/opengl/gl_quaternion.html#overview
+         * and https://danceswithcode.net/engineeringnotes/quaternions/quaternions.html)
          */
         template <typename T = float>
         constexpr void rotate (const Quaternion<T>& q)
@@ -511,7 +512,10 @@ namespace morph {
         {
             Quaternion<Flt> q;
             q.rotate (axis, theta);
+            // Two ways to get matrix. this->rotate does it as above.
             this->rotate<Flt> (q);
+            // Quaternion can write matrix into this->mat
+            // q.rotationMatrix (this->mat);
         }
 
         constexpr void rotate (const morph::vec<Flt, 3>& axis, const Flt& theta)
