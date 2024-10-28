93102971f175a19fab878691e75ebcf064c67ed2
Seb James <sjameswml@gmail.com>
2024-10-28 15:12:23 +0000

    Adds a new location for maths, initially with a simple factorial

---
+++ morph/math.h
@@ -0,0 +1,65 @@
+// Namespaced mathematical algorithms
+
+#pragma once
+
+/*
+ * Maths in a morph::math namespace
+ *
+ * Prefer this location for maths functions over the class in morph/MathAlgo.
+ */
+
+#include <cmath>
+#include <morph/mathconst.h>
+
+
+namespace morph {
+
+    namespace math {
+
+        // Return n!
+        template <typename T, typename I>
+        constexpr T factorial (const I n)
+        {
+            T fac = T{1};
+            for (I i = I{1}; i <= n; ++i) { fac *= i; }
+            return fac;
+        }
+
+        // Compute the normalization function N^l_m. If invalid l<0 is passed, return 0.
+        template <typename T, typename UI, typename I, typename std::enable_if< std::is_integral<UI>{} && !std::is_signed<UI>{} && std::is_integral<I>{} && std::is_signed<I>{}, bool>::type = true >
+        T Nlm (const UI l, const I m)
+        {
+            using mc = morph::mathconst<T>;
+            I absm = m < I{0} ? -m : m;
+            return std::sqrt ( mc::one_over_four_pi * (T{2} * static_cast<T>(l) + T{1}) * (morph::math::factorial<T, I>(l - absm) / morph::math::factorial<T, I>(l + absm)));
+        }
+
+
+        //! Wraps std::assoc_legendre, allowing signed m (abs(m) always passed to
+        //! std::assoc_legendre) NB: There is a recursive approach that can be used when computing
+        //! many associated legendre values, see Numerical Recipes p 252 (Spherical Harmonics)
+        template <typename T, typename UI, typename I, typename std::enable_if< std::is_integral<UI>{} && !std::is_signed<UI>{} && std::is_integral<I>{} && std::is_signed<I>{}, bool>::type = true >
+        T Plm (const UI l, const I m, const T x)
+        {
+            unsigned int absm = m >= 0 ? static_cast<unsigned int>(m) : static_cast<unsigned int>(-m);
+            return std::assoc_legendre (static_cast<unsigned int>(l), absm, x);
+        }
+
+        //! Compute the real spherical harmonic function
+        template <typename T, typename UI, typename I, typename std::enable_if< std::is_integral<UI>{} && !std::is_signed<UI>{} && std::is_integral<I>{} && std::is_signed<I>{}, bool>::type = true >
+        T real_spherical_harmonic (const UI l, const I m, const T _Nlm, const T phi, const T theta)
+        {
+            using mc = morph::mathconst<T>;
+            T ylm = T{0};
+            if (m > I{0}) {
+                ylm = mc::root_2 * _Nlm * std::cos (m * phi) * morph::math::Plm<T, UI, I> (l, m, std::cos (theta));
+            } else if (m < I{0}) {
+                ylm = mc::root_2 * _Nlm * std::sin (-m * phi) * morph::math::Plm<T, UI, I> (l, -m, std::cos (theta));
+            } else { // m == 0
+                ylm = _Nlm * morph::math::Plm<T, UI, I> (l, I{0}, std::cos (theta));
+            }
+            return ylm;
+        }
+
+    } // math
+} // morph
