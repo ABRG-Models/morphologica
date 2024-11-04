8eb18d7fed075f77ff1cbe856075213599cbc0f6
Seb James <sjameswml@gmail.com>
2024-11-04 16:03:08 +0000

    Adds the alternative form of Izhikevich's model, as described in Dynamical Systems in Neuroscience

--- examples/izhikevich_alt.cpp
+++ examples/izhikevich_alt.cpp
@@ -1,10 +1,8 @@
 /*
- * Compute a single Izhikevich neuron model and plot. This computes the abcd/ABC form of the
- * equations as published in Izhikevich, Simple Model of Spiking Neurons, IEEE Transactions on
- * Neural Networks, Vol. 14. No. 6, 2003.
+ * Compute a single Izhikevich neuron model and plot. This uses the formulation of the model given
+ * in Izhikevich, Dynamical Systems in Neuroscience p 273, Eqs. 8.5 and 8.6.
  */
 
-#include <morph/range.h>
 #include <morph/vvec.h>
 #include <morph/Visual.h>
 #include <morph/GraphVisual.h>
@@ -16,26 +14,32 @@ struct izhi
 {
     // state variables
     float I = 0.0f;      // Input current
-    float u = -10.0f;    // 'The refractory variable'
-    float v = -70.0f;    // 'Membrane voltage'
+    float u = 0.0f;      // 'The refractory variable'
+    float v = -60.0f;    // 'Membrane voltage'
 
-    // Parameters. In 'abc' model statement format.
+    // Parameters. These values are from the book, but the json will specify replacements
     float a = 0.03f;
-    float b = 0.193f;
-    float c = -65.0f;
-    float d = 0.05f;
+    float b = -2.0f;
+    float c = -50.0f;
+    float d = 100.0f;
 
-    float A = 0.032f;
-    float B = 4.0f;
-    float C = 113.147f;
+    float k = 0.7f;
+    float vr = -60.0f;
+    float vt = -40.0f;
 
-    float T = 0.4f;
-    float SI = 5.0f;
-    float vpeak = 30.0f;
+    float vpeak = 35.0f;
+
+    float CC = 100.0f; // rather than C, to distinguish from ABC
 
     // vdot and udot computations
-    float dv (const float _u, const float _v) { return (A*T) * _v * _v + (B*T) * _v + (C*T) - _u * T + I * (T/SI); }
-    float du (const float _u, const float _v) { return a * T * (b * _v - _u); }
+    float dv (const float _u, const float _v)
+    {
+        float Cvdot = k * (_v - vr) * (_v - vt) - _u + I;
+        return Cvdot / CC;
+    }
+
+    //float du (const float _u, const float _v) { return a * T * (b * _v - _u); }
+    float du (const float _u, const float _v) { return a * (b * (_v - vr) - _u); }
 
     // Apply one timestep of the differential equations for the model
     void step()
@@ -52,8 +56,8 @@ struct izhi
         u_nc.resize (Vn.size(), 0.0f);
         v_nc.resize (Vn.size(), 0.0f);
         for (unsigned int i = 0; i < Vn.size(); ++i) {
-            v_nc[i] = A * Vn[i] * Vn[i] + B * Vn[i] + C + I / SI;
-            u_nc[i] = Vn[i] * b;
+            v_nc[i] = k * (Vn[i] - vr) * (Vn[i] - vt) + I;
+            u_nc[i] = (Vn[i] - vr) * b ;
         }
     }
 
@@ -73,7 +77,7 @@ struct izhi
 };
 
 
-int main()
+int main (int argc, char** argv)
 {
     constexpr unsigned int N = 1000; // How many steps to run
     constexpr bool twodee = false;   // Should graphs be 2D only or rotatable?
@@ -88,7 +92,15 @@ int main()
     izhi iz;
 
     // Set izhi params from config
-    std::string jsonfile ("../examples/izhikevich.json");
+    std::string jsonfile ("../examples/izhikevich_alt.json");
+
+    bool user_file = false;
+    if (argc > 1) {
+        jsonfile = std::string (argv[1]);
+        user_file = true;
+    }
+
+    std::string title = "Izhikevich Neuron Model";
     morph::Config config(jsonfile);
     if (config.ready) {
         // Parameters
@@ -96,16 +108,26 @@ int main()
         iz.b = config.getFloat ("b", iz.b);
         iz.c = config.getFloat ("c", iz.c);
         iz.d = config.getFloat ("d", iz.d);
-        iz.A = config.getFloat ("A", iz.A);
-        iz.B = config.getFloat ("B", iz.B);
-        iz.C = config.getFloat ("C", iz.C);
-        iz.T = config.getFloat ("T", iz.C);
-        iz.SI = config.getFloat ("SI", iz.SI);
+        iz.k = config.getFloat ("k", iz.k);
+        iz.vr = config.getFloat ("vr", iz.vr);
+        iz.vt = config.getFloat ("vt", iz.vt);
         iz.vpeak = config.getFloat ("vpeak", iz.vpeak);
+        iz.CC = config.getFloat ("CC", iz.CC);
+
         // Initial values of state vars
         iz.u = config.getFloat ("u0", iz.u);
         iz.v = config.getFloat ("v0", iz.v);
-    }
+
+        iz.I = config.getFloat ("I", iz.I);
+
+        title = config.getString ("description", title);
+    } else if (!config.ready && user_file == true) {
+        throw std::runtime_error ("Failed to open json file given by user");
+    } // else use defaults
+
+    std::cout << "Model parameters:\na/b/c/d: " << iz.a << "/" << iz.b << "/" << iz.c << "/" << iz.d
+              << "\nC=" << iz.CC << " vr="  << iz.vr << " vt=" << iz.vt << " k=" << iz.k << ", vpeak=" << iz.vpeak << std::endl;
+    std::cout << "Initial state: v=" << iz.v << ", u=" << iz.u << " with I=" << iz.I << std::endl;
 
     // Run sim
     for (unsigned int i = 0; i < N; ++i) {
@@ -125,7 +147,7 @@ int main()
     morph::vvec<float> u_nc;
     morph::vvec<float> v_nc;
     morph::vvec<float> vrng;
-    vrng.linspace (-80.0f, -20.0f, 1000);
+    vrng.linspace (-70.0f, iz.vpeak, N);
     iz.nullclines (vrng, u_nc, v_nc);
 
     /*
@@ -151,13 +173,13 @@ int main()
      * Visualize results
      */
 
-    morph::Visual vis(1280, 768, "Izhikevich Neuron Model");
+    morph::Visual vis(1280, 768, title);
     vis.setSceneTrans (morph::vec<float,3>({-0.877793f, -0.281277f, -3.9f}));
     vis.lightingEffects();
 
     // Time
     morph::vvec<float> t(N, 0.0f);
-    t.linspace (0.0f, N/100.0f, N);
+    t.linspace (0.0f, N-1, N);
 
     // Set default dataset graphing styles
     morph::DatasetStyle ds;
@@ -197,11 +219,9 @@ int main()
     vis.bindmodel (gp);
     gp->twodimensional = twodee;
     gp->setsize (1.6,1.6);
-
     // Fix plotting range
     gp->setlimits_x (v_range);
     gp->setlimits_y (u_range);
-
     gp->xlabel = "v";
     gp->ylabel = "u";
 
@@ -226,7 +246,7 @@ int main()
     ds.quiver_gain = { 0.01f, 0.1f, 1.0f };
     // ...and then if the lengths should be log-scaled, call quiver_setlog()
     gp->quiver_setlog();
-    ds.quiver_colourmap.setType (morph::ColourMapType::Batlow);
+    ds.quiver_colourmap.setType (morph::ColourMapType::Jet);
     ds.quiver_conewidth = 1.8f;
     ds.quiver_arrowhead_prop = 0.35f;
     ds.quiver_thickness_gain = 1.5f;
@@ -234,6 +254,7 @@ int main()
     //ds.quiver_flagset.set (static_cast<unsigned int>(morph::quiver_flags::show_zeros));
     //ds.quiver_flagset.set (static_cast<unsigned int>(morph::quiver_flags::marker_sphere));
     gp->setdata (grid, du_dv_vecfield, ds);
+
     gp->finalize();
     vis.addVisualModel (gp);
 
