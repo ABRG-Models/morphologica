be4a52e7631b0aef74eb3668708036ce468957e3
Seb James <sjameswml@gmail.com>
2025-01-14 16:14:47 +0000

    Makes the rosenbrock example cycle with a randomised starting position each time

--- examples/rosenbrock.cpp
+++ examples/rosenbrock.cpp
@@ -23,6 +23,8 @@ FLT banana (FLT x, FLT y) {
 
 int main()
 {
+    int rtn = -1;
+
     // Set up a visual environment
     morph::Visual v(2600, 1800, "Rosenbrock bananas", {-0.8,-0.8}, {.05,.05,.05}, 2.0f, 0.01f);
     v.zNear = 0.001;
@@ -32,13 +34,10 @@ int main()
     v.lightingEffects (true);
 
     // Initialise the vertices
-    morph::vvec<morph::vvec<FLT>> i_vertices;
-    morph::vvec<FLT> v1 = {{ 0.7, 0.0 }};
-    morph::vvec<FLT> v2 = {{ 0.0, 0.6 }};
-    morph::vvec<FLT> v3 = {{ -0.6, -1.0 }};
-    i_vertices.push_back(v1);
-    i_vertices.push_back(v2);
-    i_vertices.push_back(v3);
+    morph::vvec<FLT> v1 = { 0.7, 0.0 };
+    morph::vvec<FLT> v2 = { 0.0, 0.6 };
+    morph::vvec<FLT> v3 = { -0.6, -1.0 };
+    morph::vvec<morph::vvec<FLT>> i_vertices = { v1, v2, v3 };
 
     // Add a 'triangle visual' to be visualised as three rods
     morph::vec<float> _offset = {0,0,0};
@@ -85,74 +84,89 @@ int main()
 
     // The smaller you make the threshold, the nearer the algo will get
     simp.termination_threshold = std::numeric_limits<FLT>::epsilon();
+    // You can prevent the algo getting stuck if termination_threshold is too small
+    simp.too_many_operations = 10000;
 
     // Temporary variable
-    FLT val;
+    FLT val = FLT{0};
 
-    // if using plotting, then set up the render clock
-    std::chrono::steady_clock::time_point lastrender = std::chrono::steady_clock::now();
-    std::chrono::steady_clock::time_point lastoptstep = std::chrono::steady_clock::now();
+    morph::RandUniform<float> rng(-3, 3);
 
-    // Now do the business
-    unsigned int lcount = 0;
-    while (simp.state != morph::NM_Simplex_State::ReadyToStop) {
+    while (!v.readyToFinish) {
 
-        // Perform optimisation steps slowly
-        std::chrono::steady_clock::duration sinceoptstep = std::chrono::steady_clock::now() - lastoptstep;
-        if (std::chrono::duration_cast<std::chrono::milliseconds>(sinceoptstep).count() > 250) {
-            lcount++;
-            if (simp.state == morph::NM_Simplex_State::NeedToComputeThenOrder) {
-                // 1. apply objective to each vertex
-                for (unsigned int i = 0; i <= simp.n; ++i) {
-                    simp.values[i] = banana (simp.vertices[i][0], simp.vertices[i][1]);
+        simp.reset (i_vertices);
+        // if using plotting, then set up the render clock
+        std::chrono::steady_clock::time_point lastrender = std::chrono::steady_clock::now();
+        std::chrono::steady_clock::time_point lastoptstep = std::chrono::steady_clock::now();
+
+        // Now do the business
+        unsigned int lcount = 0;
+        while (simp.state != morph::NM_Simplex_State::ReadyToStop && !v.readyToFinish) {
+
+            // Perform optimisation steps slowly
+            std::chrono::steady_clock::duration sinceoptstep = std::chrono::steady_clock::now() - lastoptstep;
+            if (std::chrono::duration_cast<std::chrono::milliseconds>(sinceoptstep).count() > 50) {
+                lcount++;
+                if (simp.state == morph::NM_Simplex_State::NeedToComputeThenOrder) {
+                    // 1. apply objective to each vertex
+                    for (unsigned int i = 0; i <= simp.n; ++i) {
+                        simp.values[i] = banana (simp.vertices[i][0], simp.vertices[i][1]);
+                    }
+                    simp.order();
+
+                } else if (simp.state == morph::NM_Simplex_State::NeedToOrder) {
+                    simp.order();
+
+                } else if (simp.state == morph::NM_Simplex_State::NeedToComputeReflection) {
+                    val = banana (simp.xr[0], simp.xr[1]);
+                    simp.apply_reflection (val);
+
+                } else if (simp.state == morph::NM_Simplex_State::NeedToComputeExpansion) {
+                    val = banana (simp.xe[0], simp.xe[1]);
+                    simp.apply_expansion (val);
+
+                } else if (simp.state == morph::NM_Simplex_State::NeedToComputeContraction) {
+                    val = banana (simp.xc[0], simp.xc[1]);
+                    simp.apply_contraction (val);
                 }
-                simp.order();
 
-            } else if (simp.state == morph::NM_Simplex_State::NeedToOrder) {
-                simp.order();
+                // Visualise the triangle defined by simp.vertices
+                // Copy data out from NM_Simplex
+                for (unsigned int i = 0; i <= simp.n; ++i) {
+                    tri_coords[i] = { simp.vertices[i][0], simp.vertices[i][1], 0.0 };
+                    tri_values[i] = simp.values[i];
+                }
+                tfvp->reinit();
 
-            } else if (simp.state == morph::NM_Simplex_State::NeedToComputeReflection) {
-                val = banana (simp.xr[0], simp.xr[1]);
-                simp.apply_reflection (val);
-
-            } else if (simp.state == morph::NM_Simplex_State::NeedToComputeExpansion) {
-                val = banana (simp.xe[0], simp.xe[1]);
-                simp.apply_expansion (val);
-
-            } else if (simp.state == morph::NM_Simplex_State::NeedToComputeContraction) {
-                val = banana (simp.xc[0], simp.xc[1]);
-                simp.apply_contraction (val);
+                lastoptstep = std::chrono::steady_clock::now();
             }
 
-            // Visualise the triangle defined by simp.vertices
-            // Copy data out from NM_Simplex
-            for (unsigned int i = 0; i <= simp.n; ++i) {
-                tri_coords[i] = { simp.vertices[i][0], simp.vertices[i][1], 0.0 };
-                tri_values[i] = simp.values[i];
+            std::chrono::steady_clock::duration sincerender = std::chrono::steady_clock::now() - lastrender;
+            if (std::chrono::duration_cast<std::chrono::milliseconds>(sincerender).count() > 17) { // 17 is about 60 Hz
+                v.poll();
+                v.render();
+                lastrender = std::chrono::steady_clock::now();
             }
-            tfvp->reinit();
-
-            lastoptstep = std::chrono::steady_clock::now();
         }
+        std::vector<FLT> thebest = simp.best_vertex();
+        FLT bestval = simp.best_value();
+        std::cout << "FINISHED! lcount=" << lcount
+                  << ". Best approximation: (" << thebest[0] << "," << thebest[1]
+                  << ") has value " << bestval << std::endl;
 
-        std::chrono::steady_clock::duration sincerender = std::chrono::steady_clock::now() - lastrender;
-        if (std::chrono::duration_cast<std::chrono::milliseconds>(sincerender).count() > 17) { // 17 is about 60 Hz
-            v.poll();
-            v.render();
-            lastrender = std::chrono::steady_clock::now();
+        // Randomly set the next start position
+        v1 = { rng.get(), rng.get() };
+        v2 = { rng.get(), rng.get() };
+        v3 = { rng.get(), rng.get() };
+        i_vertices = { v1, v2, v3 };
+
+        if (abs(thebest[0] - 1.0) < 1e-3 // Choose 1e-3 so that this will succeed with floats or doubles
+            && abs(thebest[1] - 1.0) < 1e-3) {
+            std::cout << "Test success" << std::endl;
+            rtn = 0;
+        } else {
+            rtn = -1;
         }
-    }
-    std::vector<FLT> thebest = simp.best_vertex();
-    FLT bestval = simp.best_value();
-    std::cout << "FINISHED! lcount=" << lcount
-              << ". Best approximation: (" << thebest[0] << "," << thebest[1]
-              << ") has value " << bestval << std::endl;
-
-    int rtn = -1;
-    if (abs(thebest[0] - 1.0) < 1e-3 // Choose 1e-3 so that this will succeed with floats or doubles
-        && abs(thebest[1] - 1.0) < 1e-3) {
-        std::cout << "Test success" << std::endl;
-        rtn = 0;
     }
 
     v.keepOpen();
