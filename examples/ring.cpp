2f3bb6dc8f05f4c1e917c04b7525f9ed6ac4f4af
Seb James <sjameswml@gmail.com>
2025-02-11 12:55:46 +0000

    The simple RingVisual used in examples/ring.cpp is generally useful

--- examples/ring.cpp
+++ examples/ring.cpp
@@ -1,33 +1,14 @@
 /*
- * Visualize a ring with VisualModel::computeRing
+ * Visualize a ring with morph::RingVisual
  */
 #include <morph/Visual.h>
-#include <morph/VisualModel.h>
+#include <morph/RingVisual.h>
 #include <morph/vec.h>
 #include <morph/ColourMap.h>
 #include <iostream>
 #include <stdexcept>
 #include <string>
 #include <sstream>
-
-// A simple ring class
-template <int glver = morph::gl::version_4_1>
-class RingVisual : public morph::VisualModel<glver>
-{
-public:
-    RingVisual(const morph::vec<float> _offset) : morph::VisualModel<glver>::VisualModel (_offset) {}
-
-    void initializeVertices()
-    {
-        this->computeRing (this->locn, this->clr, this->radius, this->thickness, this->segments);
-    }
-
-    std::array<float, 3> clr = morph::colour::goldenrod;
-    morph::vec<float, 3> locn = { 0.0f };
-    float radius = 1.0f;
-    float thickness = 0.2f;
-    int segments = 80;
-};
 
 int main()
 {
@@ -37,7 +18,7 @@ int main()
     morph::vec<int, 6> segs = {3, 4, 6, 8, 12, 24};
     morph::vec<float, 3> offset = { -6.0f, 0.0f, 0.0f };
     for (int i = 0; i < 6; ++i) {
-        auto rvm = std::make_unique<RingVisual<>> (offset);
+        auto rvm = std::make_unique<morph::RingVisual<>> (offset);
         v.bindmodel (rvm);
         rvm->clr = cmap.convert (i/6.0f);
         rvm->segments = segs[i];
