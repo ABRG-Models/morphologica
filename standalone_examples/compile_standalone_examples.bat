5be4f8b20196037fc2a9bd32467d20197880f172
Emilian R <92378685+rremilian@users.noreply.github.com>
2025-03-03 17:42:59 +0200

    Also add GITHUB_WORKSPACE variable

--- standalone_examples/compile_standalone_examples.bat
+++ standalone_examples/compile_standalone_examples.bat
@@ -9,7 +9,7 @@ for /d %%d in (*) do (
     mklink /d morphologica ..\..\..\morphologica
     if not exist build mkdir build
     pushd build
-    cmake .. -DCMAKE_TOOLCHAIN_FILE=..\vcpkg_repo\scripts\buildsystems\vcpkg.cmake
+    cmake .. -DCMAKE_TOOLCHAIN_FILE=%GITHUB_WORKSPACE%\vcpkg_repo\scripts\buildsystems\vcpkg.cmake
     make
     if errorlevel 1 (
         set "failed_examples=!failed_examples! %%d"
