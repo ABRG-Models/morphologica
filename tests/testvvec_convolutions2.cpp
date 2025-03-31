4337bdbdc6576833c074415910075f0aed8f2f75
Seb James <sjameswml@gmail.com>
2025-03-31 17:46:41 +0100

    Enables more tests

--- tests/testvvec_convolutions2.cpp
+++ tests/testvvec_convolutions2.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     int rtn = 0;
-    // Test convolve
+    // Test convolve with non-odd kernel which is now size 4 elements
     morph::vvec<float> a = { 1, 2, 3 };
     morph::vvec<float> b = { 2, 3, 2, 4 };
     morph::vvec<float> r1expct = { 14, 17, 14 };
@@ -26,22 +26,24 @@ int main()
     std::cout << aa << " * " << b << " = " << r2  << " (wrap, no centre, no resize, expect " << r2expct << ")" << std::endl;
     if (r2 != r2expct) {  std::cout << "r2 " << r2 << " != " << r2expct << std::endl; rtn -= 1; }
 
-#if 0
-    r2expct = { 16, 19, 14 };
-    r2 = a.convolve<morph::vvec<float>::wrapdata::wrap, // wrapping complexified if kernel > data
-                    morph::vvec<float>::centre_kernel::yes,
-                    morph::vvec<float>::resize_output::no> (b);
+    r2expct = { 34, 25, 36, 37, 33 };
+    r2 = aa.convolve<morph::vvec<float>::wrapdata::wrap, // wrapping complexified if kernel > data
+                     morph::vvec<float>::centre_kernel::yes,
+                     morph::vvec<float>::resize_output::no> (b);
     std::cout << a << " * " << b << " = " << r2  << " (wrap, centre, expect " << r2expct << ")" << std::endl;
     if (r2 != r2expct) {  std::cout << "r2 " << r2 << " != " << r2expct << std::endl; rtn -= 1; }
 
+    // convolve in place equivalent:
+    r2 = aa;
+    r2.convolve_inplace<morph::vvec<float>::wrapdata::wrap,
+                        morph::vvec<float>::centre_kernel::yes,
+                        morph::vvec<float>::resize_output::no> (b);
+    if (r2 != r2expct) { std::cout << "r2 inplace " << r2 << " != " << r2expct << std::endl; rtn -= 1; }
 
     // Test convolve in place
     r1 = a;
-    r2 = a;
     r1.convolve_inplace (b);
-    r2.convolve_inplace<morph::vvec<float>::wrapdata::wrap> (b);
     if (r1 != r1expct) { std::cout << "r1 inplace " << r1 << " != " << r1expct << std::endl; rtn -= 1; }
-    if (r2 != r2expct) { std::cout << "r2 inplace " << r2 << " != " << r2expct << std::endl; rtn -= 1; }
 
     // Pure maths convolution
     morph::vvec<float> a2 = {4, 5, 6}; // 3blue1brown example kernel!
@@ -50,7 +52,7 @@ int main()
                                              morph::vvec<float>::resize_output::yes> (a2);
     morph::vvec<float> threeb1b_expct = { 4, 13, 28, 27, 18 };
     std::cout <<  "pure convolution " << a << " * " << a2 << " = " << threeb1b << " (expect " << threeb1b_expct << ")\n";
-#endif
+
     std::cout << (rtn ? "FAIL\n" : "PASS\n");
     return rtn;
 }
